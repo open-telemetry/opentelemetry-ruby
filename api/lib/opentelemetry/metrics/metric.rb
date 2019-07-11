b8fb5435a0458269cf5b5d106ddfbd4c7c6acb80
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2019-07-11 10:50:16 -0400

    Remove constant

--- api/lib/opentelemetry/metrics/metric.rb
+++ api/lib/opentelemetry/metrics/metric.rb
@@ -14,17 +14,12 @@ module OpenTelemetry
         @label_keys_size = label_keys_size
       end
 
-      def default_time_series
-        self.class.TIME_SERIES
-      end
-
       # TODO: spec calls this 'get_or_create_time_series', which seems excessively wordy.
-      # TODO: I experimented with 'time_series_for(*label_values)', but that allocates an empty array if no arguments are provided.
-      def time_series_for(label_values: nil)
+      def time_series_for(label_values)
         raise ArgumentError if @label_keys_size != (label_values&.size || 0)
         raise ArgumentError if label_values&.any?(nil)
 
-        self.class.TIME_SERIES
+        default_time_series
       end
 
       def remove_time_series(label_values)
@@ -40,22 +35,30 @@ module OpenTelemetry
 
     # DoubleCounter metric aggregates instantaneous non-integral values. Cumulative values can go up or stay the same, but can never go down. Cumulative values cannot be negative.
     class DoubleCounter < Metric
-      TIME_SERIES = DoubleCounterTimeSeries.new
+      def default_time_series
+        DoubleCounterTimeSeries.new
+      end
     end
 
     # LongCounter metric aggregates instantaneous integral values. Cumulative values can go up or stay the same, but can never go down. Cumulative values cannot be negative.
     class LongCounter < Metric
-      TIME_SERIES = LongCounterTimeSeries.new
+      def default_time_series
+        LongCounterTimeSeries.new
+      end
     end
 
     # DoubleGauge metric aggregates instantaneous non-integral values. Cumulative value can go both up and down. DoubleGauge values can be negative.
     class DoubleGauge < Metric
-      TIME_SERIES = DoubleGaugeTimeSeries.new
+      def default_time_series
+        DoubleGaugeTimeSeries.new
+      end
     end
 
     # LongGauge metric aggregates instantaneous integral values. Cumulative value can go both up and down. LongGauge values can be negative.
     class LongGauge < Metric
-      TIME_SERIES = LongGaugeTimeSeries.new
+      def default_time_series
+        LongGaugeTimeSeries.new
+      end
     end
   end
 end
