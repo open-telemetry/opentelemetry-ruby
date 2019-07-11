7523228157d217bfde5f36ffade34b04cae4e11e
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2019-07-11 10:53:25 -0400

    Remove type check for Double

--- api/lib/opentelemetry/metrics/time_series.rb
+++ api/lib/opentelemetry/metrics/time_series.rb
@@ -13,21 +13,6 @@ module OpenTelemetry
       def set(value); end
 
       # @api private
-      module Double
-        def add(value)
-          raise ArgumentError if value.integer?
-
-          super
-        end
-
-        def set(value)
-          raise ArgumentError if value.integer?
-
-          super
-        end
-      end
-
-      # @api private
       module Long
         def add(value)
           raise ArgumentError unless value.integer?
@@ -51,28 +36,20 @@ module OpenTelemetry
         end
       end
 
-      # @api private
-      module Gauge
-      end
-
-      private_constant(:Gauge, :Counter, :Long, :Double)
+      private_constant(:Counter, :Long)
     end
 
     # A time series for a gauge of type double.
     class DoubleGaugeTimeSeries < TimeSeries
-      include Double
-      include Gauge
     end
 
     # A time series for a gauge of type long.
     class LongGaugeTimeSeries < TimeSeries
       include Long
-      include Gauge
     end
 
     # A time series for a counter of type double.
     class DoubleCounterTimeSeries < TimeSeries
-      include Double
       include Counter
     end
 
