f50b897ea86ef194a3d2e53fa2cb029941e6c963
Matthew Wear <matthew.wear@gmail.com>
2019-08-01 17:50:26 -0700

    Add documentation to resource

--- api/lib/opentelemetry/resources/resource.rb
+++ api/lib/opentelemetry/resources/resource.rb
@@ -9,8 +9,16 @@ module OpenTelemetry
     # Resource represents a resource, which captures identifying information about the entities
     # for which telemetry (metrics or traces) is reported.
     class Resource
+      # Returns the labels for this {Resource}
+      #
+      # @return [Hash<String, String>]
       attr_reader :labels
 
+      # Returns a newly created {Resource} with the specified labels
+      #
+      # @param [Hash<String, String>] kvs Hash of key-value pairs to be used
+      #   as labels for this resource
+      # @return [Resource]
       def initialize(kvs)
         # TODO: how defensive should we be here?
         @labels = kvs.each_with_object({}) do |(k, v), memo|
@@ -18,6 +26,13 @@ module OpenTelemetry
         end
       end
 
+      # Returns a new, merged {Resource} by merging the current {Resource} with
+      # the other {Resource}. In case of a collision, the current {Resource}
+      # takes precedence
+      #
+      # @param [Resource] other The other resource to merge
+      # @return [Resource] A new resource formed by merging the current resource
+      #   with other
       def merge(other)
         raise ArgumentError unless other.is_a?(Resource)
 
