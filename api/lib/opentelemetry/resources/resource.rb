e65897719c9472d137672d567f858933333988a7
Matthew Wear <matthew.wear@gmail.com>
2019-08-07 12:32:15 -0700

    Improve Resource (#24)
    * Add test for Resource
    
    * Fix merge; initialize with hash instead of kwargs
    
    The merge implementation was not working as intialize was expecting
    kwargs, yet the internal storage of labels is a hash with string
    keys and string values. It's easiest if we accept a hash with
    keys and values in the same format as the labels are stored. So,
    I updated initialize to expect a hash with string keys and values.
    Another option would be to keep kwargs in initialize, but convert
    string keys for labels to symbols, then use ** when merging, but
    that seems like more effort than it's worth.
    
    * Update Resource#merge semantics to comply with specification
    
    * Enforce that label keys and values are strings
    
    * Make Resource.create factory method and make constructor private
    
    By introducing the create factory method we can validate user
    input and reserve the constructor for internal use with data
    that has already been processed. This allows us to save a hash
    allocation, and type checking when we merge two existing resources.
    
    * Expose enumerator for labels, rather than the underlying frozen hash
    
    This adds a layer of protection for the underlying labels data structure
    while still allowing users the ability to iterate over them.
    
    * Inline check_and_freeze_labels in create factory method

--- api/lib/opentelemetry/resources/resource.rb
+++ api/lib/opentelemetry/resources/resource.rb
@@ -9,19 +9,65 @@ module OpenTelemetry
     # Resource represents a resource, which captures identifying information about the entities
     # for which telemetry (metrics or traces) is reported.
     class Resource
-      attr_reader :labels
+      class << self
+        private :new # rubocop:disable Style/AccessModifierDeclarations
 
-      def initialize(**kvs)
-        # TODO: how defensive should we be here?
-        @labels = kvs.to_h { |k, v| [k.to_s.clone.freeze, v.to_s.clone.freeze] }.freeze
+        # Returns a newly created {Resource} with the specified labels
+        #
+        # @param [Hash<String, String>] labels Hash of key-value pairs to be used
+        #   as labels for this resource
+        # @raise [ArgumentError] If label keys and values are not strings
+        # @return [Resource]
+        def create(labels = {})
+          frozen_labels = labels.each_with_object({}) do |(k, v), memo|
+            raise ArgumentError, 'label keys and values must be strings' unless k.is_a?(String) && v.is_a?(String)
+
+            memo[-k] = -v
+          end.freeze
+
+          new(frozen_labels)
+        end
       end
 
-      # TODO: Already set labels MUST NOT be overwritten unless they are empty string.
+      # @api private
+      # The constructor is private and only for use internally by the class.
+      # Users should use the {create} factory method to obtain a {Resource}
+      # instance.
+      #
+      # @param [Hash<String, String>] frozen_labels Frozen-hash of frozen-string
+      #  key-value pairs to be used as labels for this resource
+      # @return [Resource]
+      def initialize(frozen_labels)
+        @labels = frozen_labels
+      end
+
+      # Returns an enumerator for labels of this {Resource}
+      #
+      # @return [Enumerator]
+      def label_enumerator
+        @label_enumerator ||= labels.to_enum
+      end
+
+      # Returns a new, merged {Resource} by merging the current {Resource} with
+      # the other {Resource}. In case of a collision, the current {Resource}
+      # takes precedence
+      #
+      # @param [Resource] other The other resource to merge
+      # @return [Resource] A new resource formed by merging the current resource
+      #   with other
       def merge(other)
         raise ArgumentError unless other.is_a?(Resource)
 
-        self.class.new(labels.merge(other.labels))
+        merged_labels = labels.merge(other.labels) do |_, old_v, new_v|
+          old_v.empty? ? new_v : old_v
+        end
+
+        self.class.send(:new, merged_labels.freeze)
       end
+
+      protected
+
+      attr_reader :labels
     end
   end
 end
---
+++ api/test/resources/resource_test.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+# Copyright 2019 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'test_helper'
+
+describe OpenTelemetry::Resources::Resource do
+  describe '.new' do
+    it 'is private' do
+      -> { OpenTelemetry::Resources::Resource.new('k1' => 'v1') }\
+        .must_raise(NoMethodError)
+    end
+  end
+  describe '.create' do
+    it 'can be initialized with labels' do
+      expected_labels = { 'k1' => 'v1', 'k2' => 'v2' }
+      resource = OpenTelemetry::Resources::Resource.create(expected_labels)
+      resource.label_enumerator.to_h.must_equal(expected_labels)
+    end
+
+    it 'can be empty' do
+      resource = OpenTelemetry::Resources::Resource.create
+      resource.label_enumerator.to_h.must_be_empty
+    end
+
+    it 'enforces keys are strings' do
+      -> { OpenTelemetry::Resources::Resource.create(k1: 'v1') }\
+        .must_raise(ArgumentError)
+    end
+
+    it 'enforces values are strings' do
+      -> { OpenTelemetry::Resources::Resource.create('k1' => :v1) }\
+        .must_raise(ArgumentError)
+    end
+  end
+
+  describe '#merge' do
+    it 'merges two resources into a third' do
+      res1 = OpenTelemetry::Resources::Resource.create('k1' => 'v1',
+                                                       'k2' => 'v2')
+      res2 = OpenTelemetry::Resources::Resource.create('k3' => 'v3',
+                                                       'k4' => 'v4')
+      res3 = res1.merge(res2)
+
+      res3.label_enumerator.to_h.must_equal('k1' => 'v1', 'k2' => 'v2',
+                                            'k3' => 'v3', 'k4' => 'v4')
+      res1.label_enumerator.to_h.must_equal('k1' => 'v1', 'k2' => 'v2')
+      res2.label_enumerator.to_h.must_equal('k3' => 'v3', 'k4' => 'v4')
+    end
+
+    it 'does not overwrite receiver\'s keys when value is non-empty' do
+      res1 = OpenTelemetry::Resources::Resource.create('k1' => 'v1',
+                                                       'k2' => 'v2')
+      res2 = OpenTelemetry::Resources::Resource.create('k2' => '2v2',
+                                                       'k3' => '2v3')
+      res3 = res1.merge(res2)
+
+      res3.label_enumerator.to_h.must_equal('k1' => 'v1',
+                                            'k2' => 'v2',
+                                            'k3' => '2v3')
+    end
+
+    it 'overwrites receiver\'s key when value is empty' do
+      res1 = OpenTelemetry::Resources::Resource.create('k1' => 'v1',
+                                                       'k2' => '')
+      res2 = OpenTelemetry::Resources::Resource.create('k2' => '2v2',
+                                                       'k3' => '2v3')
+      res3 = res1.merge(res2)
+
+      res3.label_enumerator.to_h.must_equal('k1' => 'v1',
+                                            'k2' => '2v2',
+                                            'k3' => '2v3')
+    end
+  end
+end
