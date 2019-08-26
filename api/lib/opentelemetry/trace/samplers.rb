212acf1c39e5a94eb31c4ea3eefbbfdeda33cf62
Matthew Wear <matthew.wear@gmail.com>
2019-08-26 12:01:05 -0700

    Sampling API (#52)
    * Add BasicDecision class
    
    * Add Decision class
    
    * Add AlwaysSampleSampler
    
    * Add NeverSampleSampler
    
    * Document the Samplers module
    
    * Expect links to be a possibly nil Enumerable of Link objects
    
    * Add Sampler abstract class with type checking
    
    * Subclass Sampler and check arguments for Always/NeverSampleSampler
    
    * Treat Decision#attributes as immutable; remove BasicDecision
    
    * Be more consistent with parentheses usage
    
    * Ensure attributes is a hash when initialized with nil
    
    * Document that span_context, extracted_context can be nil for a root span
    
    * Inline check_arguments to Sampler#should_sample
    
    * Rename Sampler#should_sample -> Sampler#decision
    
    * Test Decision#attributes are frozen
    
    * Documentation fixes

---
+++ api/lib/opentelemetry/trace/samplers.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Copyright 2019 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'opentelemetry/trace/samplers/sampler'
+require 'opentelemetry/trace/samplers/always_sample_sampler'
+require 'opentelemetry/trace/samplers/never_sample_sampler'
+require 'opentelemetry/trace/samplers/decision'
+
+module OpenTelemetry
+  module Trace
+    # The Samplers module contains the sampling logic for OpenTelemetry. The
+    # minimal implementation provides a {AlwaysSampleSampler} and a
+    # {NeverSampleSampler}.
+    module Samplers
+    end
+  end
+end
