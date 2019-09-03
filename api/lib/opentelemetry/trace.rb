fe512a65a33da3993709eb29894c854aa8e8df40
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2019-09-03 11:02:12 -0400

    Add TraceFlags class

--- api/lib/opentelemetry/trace.rb
+++ api/lib/opentelemetry/trace.rb
@@ -4,9 +4,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+require 'opentelemetry/trace/samplers'
 require 'opentelemetry/trace/span_context'
 require 'opentelemetry/trace/span_kind'
 require 'opentelemetry/trace/span'
 require 'opentelemetry/trace/status'
+require 'opentelemetry/trace/trace_flags'
 require 'opentelemetry/trace/tracer'
-require 'opentelemetry/trace/samplers'
---
+++ api/test/opentelemetry/trace/trace_flags_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+describe OpenTelemetry::Trace::TraceFlags do
+  describe '.new' do
+    it 'is private' do
+      -> { OpenTelemetry::Trace::TraceFlags.new(0) }\
+        .must_raise(NoMethodError)
+    end
+  end
+  describe '.fromByte' do
+    it 'can be initialized with a byte' do
+      flags = OpenTelemetry::Trace::TraceFlags.from_byte(0)
+      flags.sampled?.must_equal(false)
+    end
+
+    it 'enforces flags is an 8-bit byte' do
+      -> { OpenTelemetry::Trace::TraceFlags.from_byte(256) }\
+        .must_raise(ArgumentError)
+    end
+  end
+
+  describe '#sampled?' do
+    it 'reflects the least-significant bit in the flags' do
+      sampled = OpenTelemetry::Trace::TraceFlags.from_byte(1)
+      not_sampled = OpenTelemetry::Trace::TraceFlags.from_byte(0)
+
+      sampled.sampled?.must_equal(true)
+      not_sampled.sampled?.must_equal(false)
+    end
+  end
+end
