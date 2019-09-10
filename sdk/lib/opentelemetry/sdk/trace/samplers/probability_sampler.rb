64105d8fc6588acfe40499aacc5993c716861b0c
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2019-09-10 14:26:47 -0400

    Implement ProbabilitySampler (#60)

--- sdk/lib/opentelemetry/sdk/trace/samplers/probability_sampler.rb
+++ sdk/lib/opentelemetry/sdk/trace/samplers/probability_sampler.rb
@@ -8,8 +8,87 @@ module OpenTelemetry
   module SDK
     module Trace
       module Samplers
-        class ProbabilitySampler
-          # TODO
+        # A {OpenTelemetry::Trace::Samplers::Sampler} where the probability of
+        # sampling a trace is equal to that of the specified probability.
+        class ProbabilitySampler < OpenTelemetry::Trace::Samplers::Sampler
+          class << self
+            private :new # rubocop:disable Style/AccessModifierDeclarations
+
+            # Returns a new {OpenTelemetry::Trace::Samplers::Sampler}. The
+            # probability of sampling a trace is equal to that of the specified
+            # probability.
+            #
+            # @param [Numeric] probability The desired probability of sampling.
+            #   Must be within [0.0, 1.0].
+            # @raise [ArgumentError] if probability is out of range
+            # @return [OpenTelemetry::Trace::Samplers::Sampler]
+            def create(probability)
+              raise ArgumentError, 'probability must be in range [0.0, 1.0]' unless (0.0..1.0).include?(probability)
+
+              return OpenTelemetry::Trace::Samplers::AlwaysSampleSampler.new if probability == 1.0
+
+              new(probability)
+            end
+          end
+
+          # @api private
+          # The constructor is private and only for use internally by the
+          # class. Users should use the {create} factory method to obtain a
+          # {ProbabilitySampler} instance.
+          #
+          # @param [Numeric] probability The desired probability of sampling.
+          #   Must be within [0.0, 1.0].
+          # @return [ProbabilitySampler]
+          def initialize(probability)
+            @probability = probability
+          end
+
+          SAMPLE_DECISION = OpenTelemetry::Trace::Samplers::Decision.new(decision: true)
+          DONT_SAMPLE_DECISION = OpenTelemetry::Trace::Samplers::Decision.new(decision: false)
+
+          private_constant(:SAMPLE_DECISION, :DONT_SAMPLE_DECISION)
+
+          # Returns the sampling {OpenTelemetry::Trace::Samplers::Decision} for a
+          # {Span} to be created
+          #
+          # @param [SpanContext] span_context The
+          #   {OpenTelemetry::Trace::SpanContext} of a parent span, typically
+          #   extracted from the wire. Can be nil for a root span.
+          # @param [Boolean] extracted_context True if span_context was extracted
+          #   from the wire. Can be nil for a root span.
+          # @param [Integer] trace_id The trace_id of the {Span} to be created
+          # @param [Integer] span_id The span_id of the {Span} to be created
+          # @param [String] span_name Name of the {Span} to be created
+          # @param [Enumerable<Link>] links A collection of links to be associated
+          #   with the {Span} to be created. Can be nil.
+          # @return [Decision] The sampling decision
+          def decision(span_context: nil,
+                       extracted_context: nil,
+                       trace_id:,
+                       span_id:,
+                       span_name:,
+                       links: nil)
+            super
+
+            # If the parent is sampled keep the sampling decision.
+            if span_context&.trace_flags&.sampled?
+              SAMPLE_DECISION
+            elsif links&.any? { |link| link.context.trace_flags.sampled? }
+              # If any parent link is sampled keep the sampling decision.
+              SAMPLE_DECISION
+            elsif rand < @probability
+              SAMPLE_DECISION
+            else
+              DONT_SAMPLE_DECISION
+            end
+          end
+
+          # Returns a description of the sampler
+          #
+          # @return [String]
+          def description
+            format('ProbabilitySampler{%.6f}', @probability)
+          end
         end
       end
     end
--- sdk/test/opentelemetry/sdk/trace/samplers/probability_sampler_test.rb
+++ sdk/test/opentelemetry/sdk/trace/samplers/probability_sampler_test.rb
@@ -7,4 +7,70 @@
 require 'test_helper'
 
 describe OpenTelemetry::SDK::Trace::Samplers::ProbabilitySampler do
+  ProbabilitySampler = OpenTelemetry::SDK::Trace::Samplers::ProbabilitySampler
+  Sampler = OpenTelemetry::Trace::Samplers::Sampler
+  SpanContext = OpenTelemetry::Trace::SpanContext
+  TraceFlags = OpenTelemetry::Trace::TraceFlags
+  Link = OpenTelemetry::Trace::Link
+  Decision = OpenTelemetry::Trace::Samplers::Decision
+
+  describe '#decision' do
+    let(:context) { SpanContext.new(trace_flags: TraceFlags.from_byte(1)) }
+    let(:sampler) { ProbabilitySampler.create(Float::MIN) }
+    it 'respects parent sample decision' do
+      decision = sampler.decision(
+        trace_id: 123,
+        span_id: 456,
+        span_name: '',
+        span_context: context
+      )
+      decision.must_be :sampled?
+    end
+    it 'respects link sample decisions' do
+      link = OpenTelemetry::Trace::Link.new(span_context: context)
+      decision = sampler.decision(
+        trace_id: 123,
+        span_id: 456,
+        span_name: '',
+        links: [link]
+      )
+      decision.must_be :sampled?
+    end
+    it 'returns a decision' do
+      decision = sampler.decision(trace_id: 123, span_id: 456, span_name: '')
+      decision.must_be_instance_of(Decision)
+    end
+    it 'returns a true decision if probability is 1' do
+      positive = ProbabilitySampler.create(1)
+      decision = positive.decision(trace_id: 123, span_id: 456, span_name: '')
+      decision.must_be :sampled?
+    end
+    it 'returns a false decision if probability is 0' do
+      negative = ProbabilitySampler.create(0)
+      decision = negative.decision(trace_id: 123, span_id: 456, span_name: '')
+      decision.wont_be :sampled?
+    end
+  end
+
+  describe '#description' do
+    let(:sampler) { ProbabilitySampler.create(0.4) }
+    it 'returns a String' do
+      sampler.description.must_be_kind_of(String)
+    end
+  end
+
+  describe '.create' do
+    it 'limits probability to the range (0...1)' do
+      proc { ProbabilitySampler.create(-1) }.must_raise(ArgumentError)
+      ProbabilitySampler.create(0).wont_be_nil
+      ProbabilitySampler.create(0.5).wont_be_nil
+      ProbabilitySampler.create(1).wont_be_nil
+      proc { ProbabilitySampler.create(2) }.must_raise(ArgumentError)
+    end
+    it 'returns a Sampler' do
+      ProbabilitySampler.create(0).must_be_kind_of(Sampler)
+      ProbabilitySampler.create(0.5).must_be_kind_of(Sampler)
+      ProbabilitySampler.create(1).must_be_kind_of(Sampler)
+    end
+  end
 end
