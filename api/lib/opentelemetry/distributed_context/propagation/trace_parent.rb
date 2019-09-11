859d38aaa868f264a4947d3bcf96dd1418e61c6e
Ian Quick <ian.quick@gmail.com>
2019-09-11 13:50:08 -0400

    move up the self methods into one block

--- api/lib/opentelemetry/distributed_context/propagation/trace_parent.rb
+++ api/lib/opentelemetry/distributed_context/propagation/trace_parent.rb
@@ -24,67 +24,36 @@ module OpenTelemetry
         REGEXP = /^(?<version>[A-Fa-f0-9]{2})-(?<trace_id>[A-Fa-f0-9]{32})-(?<span_id>[A-Fa-f0-9]{16})-(?<flags>[A-Fa-f0-9]{2})(?<ignored>-.*)?$/.freeze
         private_constant :REGEXP
 
-        attr_reader :version, :trace_id, :span_id, :flags
-
-        private_class_method :new
-
-        # Returns the sampling choice from the trace_flags
-        # @return [Boolean] the sampling choice
-        def sampled?
-          flags.sampled?
-        end
-
-        # converts this object into a string according to the w3c spec
-        # @return [String] the serialized trace_parent
-        def to_s
-          "00-#{trace_id}-#{span_id}-#{flag_string}"
-        end
-
-        # Creates a new {TraceParent} from a supplied {SpanContext}
-        # @param [SpanContext] the context
-        # @return [TraceParent] a trace parent
-        def self.from_context(ctx)
-          new(trace_id: ctx.trace_id, span_id: ctx.span_id, flags: ctx.trace_flags)
-        end
-
-        # Deserializes the {TraceParent} from the string representation
-        # @param [String] the serialized trace parent
-        # @return [TraceParent] a trace_parent
-        # @raise [InvalidFormatError] on an invalid format
-        # @raise [InvalidVerionError] on an invalid version
-        # @raise [InvalidTraceIDError] on an invalid trace_id
-        # @raise [InvalidSpanIDError] on an invalid span_id
-        def self.from_string(string)
-          matches = match_input(string)
-
-          version = parse_version(matches[:version])
-          raise InvalidFormatError if version > SUPPORTED_VERSION && string.length < 55
-
-          trace_id = parse_trace_id(matches[:trace_id])
-          span_id = parse_span_id(matches[:span_id])
-          flags = parse_flags(matches[:flags])
-
-          new(trace_id: trace_id, span_id: span_id, flags: flags)
-        end
-
-        private
-
-        def flag_string
-          # the w3c standard only dictates the one flag for this version
-          # therefore we can only output the one flag.
-          flags.sampled? ? '01' : '00'
-        end
-
-        def initialize(trace_id: nil, span_id: nil, version: SUPPORTED_VERSION, flags: Trace::TraceFlags::DEFAULT)
-          raise ArgumentError, 'flags must be a TraceFlags' unless flags.is_a?(Trace::TraceFlags)
-
-          @trace_id = trace_id
-          @span_id = span_id
-          @version = version
-          @flags = flags
-        end
-
         class << self
+          # Creates a new {TraceParent} from a supplied {SpanContext}
+          # @param [SpanContext] the context
+          # @return [TraceParent] a trace parent
+          def from_context(ctx)
+            new(trace_id: ctx.trace_id, span_id: ctx.span_id, flags: ctx.trace_flags)
+          end
+
+          # Deserializes the {TraceParent} from the string representation
+          # @param [String] the serialized trace parent
+          # @return [TraceParent] a trace_parent
+          # @raise [InvalidFormatError] on an invalid format
+          # @raise [InvalidVerionError] on an invalid version
+          # @raise [InvalidTraceIDError] on an invalid trace_id
+          # @raise [InvalidSpanIDError] on an invalid span_id
+          def from_string(string)
+            matches = match_input(string)
+
+            version = parse_version(matches[:version])
+            raise InvalidFormatError if version > SUPPORTED_VERSION && string.length < 55
+
+            trace_id = parse_trace_id(matches[:trace_id])
+            span_id = parse_span_id(matches[:span_id])
+            flags = parse_flags(matches[:flags])
+
+            new(trace_id: trace_id, span_id: span_id, flags: flags)
+          end
+
+          private
+
           def match_input(string)
             matches = REGEXP.match(string)
             raise InvalidFormatError, 'regexp match failed' if !matches || matches.length < 6
@@ -117,6 +86,39 @@ module OpenTelemetry
           def parse_flags(string)
             OpenTelemetry::Trace::TraceFlags.from_byte(string.to_i(16))
           end
+        end
+
+        attr_reader :version, :trace_id, :span_id, :flags
+
+        private_class_method :new
+
+        # Returns the sampling choice from the trace_flags
+        # @return [Boolean] the sampling choice
+        def sampled?
+          flags.sampled?
+        end
+
+        # converts this object into a string according to the w3c spec
+        # @return [String] the serialized trace_parent
+        def to_s
+          "00-#{trace_id}-#{span_id}-#{flag_string}"
+        end
+
+        private
+
+        def flag_string
+          # the w3c standard only dictates the one flag for this version
+          # therefore we can only output the one flag.
+          flags.sampled? ? '01' : '00'
+        end
+
+        def initialize(trace_id: nil, span_id: nil, version: SUPPORTED_VERSION, flags: Trace::TraceFlags::DEFAULT)
+          raise ArgumentError, 'flags must be a TraceFlags' unless flags.is_a?(Trace::TraceFlags)
+
+          @trace_id = trace_id
+          @span_id = span_id
+          @version = version
+          @flags = flags
         end
       end
     end
