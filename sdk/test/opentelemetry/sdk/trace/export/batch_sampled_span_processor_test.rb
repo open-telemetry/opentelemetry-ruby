1a9527ac92c1fed0f0bb57af0d4981f5e6b2e0ae
Ian Quick <ian.quick@gmail.com>
2019-09-11 09:18:04 -0400

    a simple test

--- sdk/test/opentelemetry/sdk/trace/export/batch_sampled_span_processor_test.rb
+++ sdk/test/opentelemetry/sdk/trace/export/batch_sampled_span_processor_test.rb
@@ -7,4 +7,22 @@
 require 'test_helper'
 
 describe OpenTelemetry::SDK::Trace::Export::BatchSampledSpanProcessor do
+  BatchSampledSpanProcessor = OpenTelemetry::SDK::Trace::MultiSpanProcessor
+
+  class TestExporter
+    def export(batch)
+      batches << batch
+    end
+
+    def batches
+      @batches ||= []
+    end
+  end
+
+  describe 'lifecycle' do
+    it 'should stop and start correctly' do
+      bsp = BatchSampledSpanProcessor.new(exporter: TestExporter.new)
+      bsp.shutdown
+    end
+  end
 end
