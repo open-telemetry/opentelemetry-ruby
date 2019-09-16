c41a1a5995c15d59db9c8a6a5c31d6ec061f6ed5
Stephen Veiss <stephen.veiss@shopify.com>
2019-09-16 15:59:22 -0700

    Add tests for the basic SpanProcessor implementations

---
+++ sdk/test/opentelemetry/sdk/trace/multi_span_processor_test.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+# Copyright 2019 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'test_helper'
+
+describe OpenTelemetry::SDK::Trace::MultiSpanProcessor do
+  let(:mock_processor1)    { Minitest::Mock.new }
+  let(:mock_processor2)    { Minitest::Mock.new }
+  let(:span)               { 'dummy span' }
+
+  let(:subject) do
+    OpenTelemetry::SDK::Trace::MultiSpanProcessor.new(
+      [mock_processor1, mock_processor2]
+    )
+  end
+
+  it 'implements #on_start' do
+    mock_processor1.expect :on_start, nil, [span]
+    mock_processor2.expect :on_start, nil, [span]
+
+    subject.on_start(span)
+
+    mock_processor1.verify
+    mock_processor2.verify
+  end
+
+  it 'implements #on_end' do
+    mock_processor1.expect :on_end, nil, [span]
+    mock_processor2.expect :on_end, nil, [span]
+
+    subject.on_end(span)
+
+    mock_processor1.verify
+    mock_processor2.verify
+  end
+
+  it 'implements #shutdown' do
+    mock_processor1.expect :shutdown, nil
+    mock_processor2.expect :shutdown, nil
+
+    subject.shutdown
+
+    mock_processor1.verify
+    mock_processor2.verify
+  end
+end
--- sdk/test/opentelemetry/sdk/trace/noop_span_processor_test.rb
+++ sdk/test/opentelemetry/sdk/trace/noop_span_processor_test.rb
@@ -7,4 +7,18 @@
 require 'test_helper'
 
 describe OpenTelemetry::SDK::Trace::NoopSpanProcessor do
+  let(:subject) { OpenTelemetry::SDK::Trace::NoopSpanProcessor.instance }
+  let(:span)    { nil }
+
+  it 'implements #on_start' do
+    subject.on_start(span)
+  end
+
+  it 'implements #on_end' do
+    subject.on_end(span)
+  end
+
+  it 'implements #shutdown' do
+    subject.shutdown
+  end
 end
