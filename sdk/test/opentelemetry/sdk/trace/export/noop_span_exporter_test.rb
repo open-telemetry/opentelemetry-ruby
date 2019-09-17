eeae3ab0db28e18a0cfa5bb15f4ae648d97aa9e4
Stephen Veiss <stephen.veiss@shopify.com>
2019-09-17 12:26:24 -0700

    Add tests for NoopSpanExporter

--- sdk/test/opentelemetry/sdk/trace/export/noop_span_exporter_test.rb
+++ sdk/test/opentelemetry/sdk/trace/export/noop_span_exporter_test.rb
@@ -7,4 +7,26 @@
 require 'test_helper'
 
 describe OpenTelemetry::SDK::Trace::Export::NoopSpanExporter do
+  sdk = OpenTelemetry::SDK
+  export = OpenTelemetry::SDK::Trace::Export
+
+  let(:spans)   { [sdk::Trace::Span.new, sdk::Trace::Span.new] }
+  let(:subject) { export::NoopSpanExporter.new }
+
+  it 'accepts an Array of Spans as arg to #export and succeeds' do
+    subject.export(spans).must_equal export::SUCCESS
+  end
+
+  it 'accepts an Enumerable of Spans as arg to #export and succeeds' do
+    # An anonymous Struct serves as a handy implementor of Enumerable
+    enumerable = Struct.new(:span0, :span1).new
+    enumerable.span0 = spans[0]
+    enumerable.span1 = spans[1]
+
+    subject.export(enumerable).must_equal export::SUCCESS
+  end
+
+  it 'accepts calls to #shutdown' do
+    subject.shutdown
+  end
 end
