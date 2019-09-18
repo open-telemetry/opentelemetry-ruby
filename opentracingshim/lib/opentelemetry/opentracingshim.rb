2d91407579c30501d58b77a4e52429d01f99976f
Rees Dooley <rees.dooley@shopify.com>
2019-09-18 08:45:07 -0700

    Stub out opentracingshim

---
+++ opentracingshim/lib/opentelemetry/opentracingshim.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Copyright 2019 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'opentelemetry'
+
+module OpenTelemetry
+  # OpenTracingShim provides a means of converting
+  # from OpenTelemetry to OpenTracing objects
+  module OpenTracingShim
+  end
+end
+
+# require 'opentelemetry/opentracingshim/internal'
+# require 'opentelemetry/opentracingshim/trace'
+# require 'opentelemetry/opentracingshim/version'
---
+++ opentracingshim/test/test_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Copyright 2019 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'simplecov'
+SimpleCov.start
+
+require 'opentelemetry/opentracingshim'
+require 'minitest/autorun'
