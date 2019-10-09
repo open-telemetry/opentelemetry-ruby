3265f5a50c5226e42463f4bf415187f49e02e16d
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2019-10-09 14:25:29 -0400

    Implement TracerFactory (#114)

---
+++ api/test/opentelemetry/trace/tracer_factory_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Copyright 2019 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'test_helper'
+
+describe OpenTelemetry::Trace::TracerFactory do
+  let(:tracer_factory) { OpenTelemetry::Trace::TracerFactory.new }
+
+  describe '.tracer' do
+    it 'returns the same tracer for the same arguments' do
+      tracer1 = tracer_factory.tracer('component', 'semver:1.0')
+      tracer2 = tracer_factory.tracer('component', 'semver:1.0')
+      _(tracer1).must_equal(tracer2)
+    end
+  end
+end
