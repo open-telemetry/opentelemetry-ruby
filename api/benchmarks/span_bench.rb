963c22f2667a49cb56fa1aeba5aa86aa79058e5b
Matthew Wear <matthew.wear@gmail.com>
2019-10-09 19:15:13 -0700

    Add basic benchmarks for tracer and span

---
+++ api/benchmarks/span_bench.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Copyright 2019 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'benchmark/ipsa'
+require 'opentelemetry'
+
+span = OpenTelemetry::Trace::Span.new
+
+attributes = {
+  'component' => 'rack',
+  'span.kind' => 'server',
+  'http.method' => 'GET',
+  'http.url' => 'blogs/index'
+}
+
+Benchmark.ipsa do |x|
+  x.report 'name=' do
+    span.name = 'new_name'
+  end
+
+  x.report 'set_attribute' do
+    span.set_attribute('k', 'v')
+  end
+
+  x.report 'add_event' do
+    span.add_event(name: 'test event', attributes: attributes)
+  end
+
+  x.compare!
+end
