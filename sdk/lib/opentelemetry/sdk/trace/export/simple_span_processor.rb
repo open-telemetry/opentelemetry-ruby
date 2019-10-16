3b52ae0beae634e6ab18db3b50c1bb4825b98c8f
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2019-10-16 09:40:45 -0400

    Recording vs sampled (#121)

--- sdk/lib/opentelemetry/sdk/trace/export/simple_span_processor.rb
+++ sdk/lib/opentelemetry/sdk/trace/export/simple_span_processor.rb
@@ -47,7 +47,7 @@ module OpenTelemetry
           #
           # @param [Span] span the {Span} that just ended.
           def on_finish(span)
-            return unless span.recording?
+            return unless span.context.trace_flags.sampled?
 
             @span_exporter.export([span.to_span_data])
           rescue => e # rubocop:disable Style/RescueStandardError
