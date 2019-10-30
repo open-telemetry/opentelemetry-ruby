435a1b8b927d3503c97b291e3d4d53cc062262f3
David Davis <david.davis@mutations.ltd>
2019-10-30 14:52:18 -0700

    Add test
    * redirect and capture stdout (console output)
    
    example output:
    
    "#<OpenTelemetry::Trace::Span:0xXXXXXX
     @context=
      #<OpenTelemetry::Trace::SpanContext:0xXXXXXX
       @remote=false,
       @span_id=\"646b7ead71c2aa56\",
       @trace_flags=
        #<OpenTelemetry::Trace::TraceFlags:0xXXXXXX @flags=0>,
       @trace_id=\"365b4bf2e7bcbff1d72a11d9d83f1a9d\",
       @tracestate=nil>>
    [...]

---
+++ sdk/test/opentelemetry/sdk/trace/export/console_span_exporter_test.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+# Copyright 2019 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'test_helper'
+
+describe OpenTelemetry::SDK::Trace::Export::ConsoleSpanExporter do
+  export = OpenTelemetry::SDK::Trace::Export
+
+  let(:captured_stdout) { StringIO.new }
+  let(:spans)    { [OpenTelemetry::Trace::Span.new, OpenTelemetry::Trace::Span.new] }
+  let(:exporter) { export::ConsoleSpanExporter.new }
+
+  before do
+    @original_stdout = $stdout
+    $stdout = captured_stdout
+  end
+
+  after do
+    $stdout = @original_stdout
+  end
+
+  it 'accepts an Array of Spans as arg to #export and succeeds' do
+    _(exporter.export(spans)).must_equal export::SUCCESS
+  end
+
+  it 'accepts an Enumerable of Spans as arg to #export and succeeds' do
+    enumerable = Struct.new(:span0, :span1).new
+    enumerable.span0 = spans[0]
+    enumerable.span1 = spans[1]
+
+    _(exporter.export(enumerable)).must_equal export::SUCCESS
+  end
+
+  it 'outputs to console (stdout)' do
+    exporter.export(spans)
+
+    _(captured_stdout.string).must_match(/#<OpenTelemetry::Trace::Span:/)
+  end
+
+  it 'accepts calls to #shutdown' do
+    exporter.shutdown
+  end
+
+  it 'fails to export after shutdown' do
+    exporter.shutdown
+
+    _(exporter.export(spans)).must_equal export::FAILED_NOT_RETRYABLE
+  end
+end
