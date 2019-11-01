f5b1dc0c5efe93d0d3181e1c96601be2e741d7ba
David Davis <duonoid@users.noreply.github.com>
2019-11-01 12:18:18 -0700

    Add Quick Start API documentation and examples links (#135)
    * Add Quick Start API documentation and examples links
    
    * Refine example, setting trace name and version for 'my_app_or_gem'
    
    * Add SDK::Trace::Export::ConsoleSpanExporter
    
    * SDK::Trace::Export: require 'console_span_exporter'
    
    * Add test
    
    * redirect and capture stdout (console output)
    
    example output:
    
    "#<OpenTelemetry::Trace::Span:0xXXXXXX
     @context=
      #<OpenTelemetry::Trace::SpanContext:0xXXXXXX
       @remote=false,
       @span_id=\"646b7ead71c2aa56\",
       @trace_flags=
        #<OpenTelemetry::Trace::TraceFlags:0xXXXXXX @flags=0>,
       @trace_id=\"365b4bf2e7bcbff1d72a11d9d83f1a9d\",
       @tracestate=nil>>
    [...]
    
    * README: Use ConsoleSpanExporter
    
    * Change ResultCodes to private_constant
    
    * Refactor Struct usage to be one-liner

--- sdk/test/opentelemetry/sdk/trace/export/console_span_exporter_test.rb
+++ sdk/test/opentelemetry/sdk/trace/export/console_span_exporter_test.rb
@@ -6,11 +6,21 @@
 
 require 'test_helper'
 
-describe OpenTelemetry::SDK::Trace::Export::NoopSpanExporter do
+describe OpenTelemetry::SDK::Trace::Export::ConsoleSpanExporter do
   export = OpenTelemetry::SDK::Trace::Export
 
+  let(:captured_stdout) { StringIO.new }
   let(:spans)    { [OpenTelemetry::Trace::Span.new, OpenTelemetry::Trace::Span.new] }
-  let(:exporter) { export::NoopSpanExporter.new }
+  let(:exporter) { export::ConsoleSpanExporter.new }
+
+  before do
+    @original_stdout = $stdout
+    $stdout = captured_stdout
+  end
+
+  after do
+    $stdout = @original_stdout
+  end
 
   it 'accepts an Array of Spans as arg to #export and succeeds' do
     _(exporter.export(spans)).must_equal export::SUCCESS
@@ -22,12 +32,19 @@ describe OpenTelemetry::SDK::Trace::Export::NoopSpanExporter do
     _(exporter.export(enumerable)).must_equal export::SUCCESS
   end
 
+  it 'outputs to console (stdout)' do
+    exporter.export(spans)
+
+    _(captured_stdout.string).must_match(/#<OpenTelemetry::Trace::Span:/)
+  end
+
   it 'accepts calls to #shutdown' do
     exporter.shutdown
   end
 
   it 'fails to export after shutdown' do
     exporter.shutdown
+
     _(exporter.export(spans)).must_equal export::FAILED_NOT_RETRYABLE
   end
 end
