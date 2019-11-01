279c9bdb7d26d018d10b5207c196e0e6a90d47dd
Don Morrison <elskwid@users.noreply.github.com>
2019-11-01 10:25:58 -0700

    Update Tracer#in_span to finish the Span (#118)
    * Add Tracer tests to the API
    
    Adds tests to the current API `Tracer` implementation.
    
    * Update Tracer#in_span to finish the Span at the end of the block
    
    Updates `Tracer#in_span` to call `Span#finish` at the end of the
    `in_span` block. Adds tests for both the API and SDK implementations
    to ensure the Span is finished as expected.
    
    * Fix Minitest deprecation messages in sdk batch_span_processor_test

--- api/lib/opentelemetry/trace/tracer.rb
+++ api/lib/opentelemetry/trace/tracer.rb
@@ -29,6 +29,8 @@ module OpenTelemetry
       def in_span(name, attributes: nil, links: nil, start_timestamp: nil, kind: nil, sampling_hint: nil)
         span = start_span(name, attributes: attributes, links: links, start_timestamp: start_timestamp, kind: kind, sampling_hint: sampling_hint)
         with_span(span) { |s| yield s }
+      ensure
+        span.finish
       end
 
       # Activates/deactivates the Span within the current Context, which makes the "current span"
