3a3c7e72bdef7e1d8bf445a6c9c1f98a756f7119
David Davis <david.davis@mutations.ltd>
2019-11-14 10:35:10 -0800

    Allow auto-installer to inject a tracer
    * allow (auto) installer to bind tracer to instrumentation
    * config[:tracer] will override default_tracer

--- api/lib/opentelemetry/adapter.rb
+++ api/lib/opentelemetry/adapter.rb
@@ -4,22 +4,30 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+require 'opentelemetry'
+
 module OpenTelemetry
   # The basic interface for Adapter objects
   class Adapter
     class << self
-      attr_reader :config
+      attr_reader :config,
+                  :http_formatter,
+                  :tracer
 
       def install(config = {})
         @config = config
+        @http_formatter = config[:http_formatter] || default_http_formatter
+        @tracer = config[:tracer] || default_tracer
         new.install
       end
 
-      def http_formatter
+      private
+
+      def default_http_formatter
         OpenTelemetry.tracer_factory.http_text_format
       end
 
-      def tracer
+      def default_tracer
         OpenTelemetry.tracer_factory.tracer(config[:name], config[:version])
       end
     end
