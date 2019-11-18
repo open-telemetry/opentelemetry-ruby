24568139a9f2dff763fde5043352b343280d5dd8
David Davis <david.davis@mutations.ltd>
2019-11-18 11:19:59 -0800

    Add test

---
+++ api/test/opentelemetry/adapter_test.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# Copyright 2019 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'test_helper'
+
+describe OpenTelemetry::Adapter do
+  class TestAdapter < OpenTelemetry::Adapter
+    attr_accessor :state
+    def install
+      self.state = :called
+      self
+    end
+  end
+
+  class TestAdapter2 < OpenTelemetry::Adapter
+    def install; end
+  end
+
+  describe '.install' do
+    before do
+      @test_adapter = TestAdapter.install(name: 'foo', version: 'bar')
+      TestAdapter2.install(name: 'foo2', version: 'bar2')
+    end
+
+    it 'retains configuration separately' do
+      _(TestAdapter.config[:name]).must_equal 'foo'
+      _(TestAdapter2.config[:name]).must_equal 'foo2'
+    end
+
+    it 'calls #install on subclass' do
+      _(@test_adapter.state).must_equal :called
+    end
+  end
+end
