21c8ada3a713c93675a509ed2ca512708dd43dc4
David Davis <david.davis@mutations.ltd>
2019-11-19 17:30:48 -0800

    Add test_helper and first test
    * note: adapter.install effects aren't localized, so test order
      matters
    
    Update tests to avoid passing name, version to .install
    
    * name and version are now determined internally and shouldn't usually
      be passed in explicitly

---
+++ adapters/faraday/test/opentelemetry/adapters/faraday_test.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Copyright 2019 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'test_helper'
+
+require_relative '../../../lib/opentelemetry/adapters/faraday'
+
+describe OpenTelemetry::Adapters::Faraday do
+  let(:adapter) { OpenTelemetry::Adapters::Faraday }
+  let(:exporter) { EXPORTER }
+
+  before do
+    adapter.install
+    exporter.reset
+  end
+
+  describe 'tracing' do
+    before do
+      stub_request(:any, 'example.com')
+    end
+
+    it 'before request' do
+      _(exporter.finished_spans.size).must_equal 0
+    end
+
+    it 'after request' do
+      ::Faraday.new('http://example.com').get('/')
+
+      _(exporter.finished_spans.size).must_equal 1
+    end
+  end
+end
