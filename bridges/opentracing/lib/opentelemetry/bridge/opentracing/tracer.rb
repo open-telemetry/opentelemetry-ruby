730ea9574021619264bd3e6ad0b9a68f8ec06a42
Rees Dooley <rees.dooley@shopify.com>
2019-11-21 13:16:47 -0800

    Back to where i was

--- bridges/opentracing/lib/opentelemetry/bridge/opentracing/tracer.rb
+++ bridges/opentracing/lib/opentelemetry/bridge/opentracing/tracer.rb
@@ -13,8 +13,14 @@ module OpenTelemetry
         # TEXT_FORMAT = OpenTelemetry::DistributedContext::Propagation::TextFormat.new
         # BINARY_FORMAT = OpenTelemetry::DistributedContext::Propagation::BinaryFormat.new
 
+        attr_reader :tracer
+
+        def initialize(tracer)
+          @tracer = tracer
+        end
+
         def active_span
-          OpenTelemetry.tracer.current_span
+          @tracer.current_span
         end
 
         def start_active_span(operation_name,
@@ -25,15 +31,15 @@ module OpenTelemetry
                               ignore_active_scope: false,
                               finish_on_close: true,
                               &block)
-          span = OpenTelemetry.tracer.start_span(operation_name,
-                                                 with_parent: child_of,
-                                                 attributes: tags,
-                                                 links: references,
-                                                 start_timestamp: start_time)
+          span = @tracer.start_span(operation_name,
+                                    with_parent: child_of,
+                                    attributes: tags,
+                                    links: references,
+                                    start_timestamp: start_time)
           scope = Scope.new(ScopeManager.new, span, finish_on_close)
           if block_given?
             yield scope
-            return OpenTelemetry.tracer.with_span(span, &block)
+            return @tracer.with_span(span, &block)
           end
 
           scope
@@ -46,14 +52,14 @@ module OpenTelemetry
                        tags: nil,
                        ignore_active_scope: false,
                        &block)
-          span = OpenTelemetry.tracer.start_span(operation_name,
-                                                 with_parent: child_of,
-                                                 attributes: tags,
-                                                 links: references,
-                                                 start_timestamp: start_time)
+          span = @tracer.start_span(operation_name,
+                                    with_parent: child_of,
+                                    attributes: tags,
+                                    links: references,
+                                    start_timestamp: start_time)
           if block_given?
             yield span
-            return OpenTelemetry.tracer.with_span(span, &block)
+            return @tracer.with_span(span, &block)
           end
           span
         end
--- bridges/opentracing/test/opentelemetry/bridge/opentracing/tracer_test.rb
+++ bridges/opentracing/test/opentelemetry/bridge/opentracing/tracer_test.rb
@@ -9,53 +9,76 @@ require 'test_helper'
 describe OpenTelemetry::Bridge::OpenTracing::Tracer do
   SpanContext = OpenTelemetry::Trace::SpanContext
   SpanContextBridge = OpenTelemetry::Bridge::OpenTracing::SpanContext
-  let(:tracer_bridge) { OpenTelemetry::Bridge::OpenTracing::Tracer.new }
+  let(:tracer_mock) { Minitest::Mock.new }
+  let(:tracer_bridge) { OpenTelemetry::Bridge::OpenTracing::Tracer.new tracer_mock }
   describe '#active_span' do
     it 'gets the tracers active span' do
+      tracer_mock.expect(:current_span, 'an_active_span')
+      as = tracer_bridge.active_span
+      as.must_equal 'an_active_span'
+      tracer_mock.verify
     end
   end
 
   describe '#start_span' do
     it 'calls start span on the tracer' do
+      args = ['name', { with_parent: 'parent', attributes: 'tag', links: 'refs', start_timestamp: 'now' }]
+      tracer_mock.expect(:start_span, 'an_active_span', args)
+      tracer_bridge.start_span('name', child_of: 'parent', references: 'refs', tags: 'tag', start_time: 'now')
+      tracer_mock.verify
     end
 
     it 'calls with_span if a block is given, yielding the span and returning the blocks value' do
+      args = ['name', { with_parent: 'parent', attributes: 'tag', links: 'refs', start_timestamp: 'now' }]
+      tracer_mock.expect(:start_span, 'an_active_span', args)
+      tracer_mock.expect(:with_span, 'block_value', ['an_active_span'])
+      ret = tracer_bridge.start_span('name', child_of: 'parent', references: 'refs', tags: 'tag', start_time: 'now') do |span|
+        span.must_equal 'an_active_span'
+      end
+      ret.must_equal 'block_value'
+      tracer_mock.verify
     end
 
     it 'returns the span' do
+      args = ['name', { with_parent: 'parent', attributes: 'tag', links: 'refs', start_timestamp: 'now' }]
+      tracer_mock.expect(:start_span, 'an_active_span', args)
+      span = tracer_bridge.start_span('name', child_of: 'parent', references: 'refs', tags: 'tag', start_time: 'now')
+      tracer_mock.verify
+      span.must_equal 'an_active_span'
     end
   end
 
   describe '#start_active_span' do
     it 'calls start span on the tracer and with_span to make active' do
+      args = ['name', { with_parent: 'parent', attributes: 'tag', links: 'refs', start_timestamp: 'now' }]
+      tracer_mock.expect(:start_span, 'an_active_span', args)
+      tracer_bridge.start_active_span('name', child_of: 'parent', references: 'refs', tags: 'tag', start_time: 'now')
+      tracer_mock.verify
     end
 
     it 'calls with_span if a block is given, yielding the scope and returning the blocks value' do
+      args = ['name', { with_parent: 'parent', attributes: 'tag', links: 'refs', start_timestamp: 'now' }]
+      tracer_mock.expect(:start_span, 'an_active_span', args)
+      tracer_mock.expect(:with_span, 'block_value', ['an_active_span'])
+      ret = tracer_bridge.start_active_span('name', child_of: 'parent', references: 'refs', tags: 'tag', start_time: 'now') do |scope|
+        scope.span.must_equal 'an_active_span'
+      end
+      ret.must_equal 'block_value'
+      tracer_mock.verify
     end
 
     it 'returns a scope' do
+      args = ['name', { with_parent: 'parent', attributes: 'tag', links: 'refs', start_timestamp: 'now' }]
+      tracer_mock.expect(:start_span, 'an_active_span', args)
+      scope = tracer_bridge.start_active_span('name', child_of: 'parent', references: 'refs', tags: 'tag', start_time: 'now')
+      tracer_mock.verify
+      scope.span.must_equal 'an_active_span'
     end
   end
 
   describe '#inject' do
-    it 'injects TEXT_MAP format as HTTP_TEXT_FORMAT' do
-    end
-
-    it 'injects RACK format as HTTP_TEXT_FORMAT' do
-    end
-
-    it 'injects binary format onto the context' do
-    end
   end
 
   describe '#extract' do
-    it 'extracts HTTP format from the context' do
-    end
-
-    it 'extracts rack format from the context' do
-    end
-
-    it 'extracts binary format from the context' do
-    end
   end
 end
