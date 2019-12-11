ef32bc4f3fb1c9acc3610036f7c411869174482f
Matthew Wear <matthew.wear@gmail.com>
2019-12-10 18:38:28 -0800

    Add readers for injectors / extractors to TracerFactory

--- api/test/opentelemetry/trace/tracer_factory_test.rb
+++ api/test/opentelemetry/trace/tracer_factory_test.rb
@@ -16,4 +16,34 @@ describe OpenTelemetry::Trace::TracerFactory do
       _(tracer1).must_equal(tracer2)
     end
   end
+
+  describe '#http_extractor, #rack_http_extractor' do
+    it 'returns an instance of HttpTraceContextExtractor' do
+      %i[http_extractor rack_http_extractor].each do |extractor_method|
+        extractor = tracer_factory.send(extractor_method)
+        _(extractor).must_be_instance_of(
+          Propagation::HttpTraceContextExtractor
+        )
+      end
+    end
+  end
+
+  describe '#http_injector, #rack_http_injector' do
+    it 'returns an instance of HttpTraceContextInjector' do
+      %i[http_injector rack_http_injector].each do |injector_method|
+        injector = tracer_factory.send(injector_method)
+        _(injector).must_be_instance_of(
+          Propagation::HttpTraceContextInjector
+        )
+      end
+    end
+  end
+
+  describe '#binary_format' do
+    it 'returns an instance of BinaryFormat' do
+      _(tracer_factory.binary_format).must_be_instance_of(
+        Propagation::BinaryFormat
+      )
+    end
+  end
 end
