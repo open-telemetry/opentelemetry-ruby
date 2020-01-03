f737317c246e6da9892a84eaea73a49e0d90d66e
Matthew Wear <matthew.wear@gmail.com>
2020-01-02 18:57:16 -0800

    Add present, compatible, and install blocks to adapter

--- api/lib/opentelemetry/instrumentation/adapter.rb
+++ api/lib/opentelemetry/instrumentation/adapter.rb
@@ -19,31 +19,75 @@ module OpenTelemetry
           OpenTelemetry::Instrumentation.registry.register(subclass)
         end
 
-        def adapter_name(name) # rubocop:disable Style/TrivialAccessors
-          @adapter_name = name
+        def adapter_name(adapter_name = nil)
+          adapter_name ? @adapter_name = adapter_name : @adapter_name
         end
 
-        def adapter_version(version) # rubocop:disable Style/TrivialAccessors
-          @adapter_version = version
+        def adapter_version(adapter_version = nil)
+          adapter_version ? @adapter_version = adapter_version : @adapter_version
+        end
+
+        def install(&blk)
+          @install_blk = blk
+        end
+
+        def present(&blk)
+          @present_blk = blk
+        end
+
+        def compatible(&blk)
+          @compatible_blk = blk
         end
 
         def instance
-          @instance ||= new(@adapter_name, @adapter_version)
+          @instance ||= new(adapter_name, adapter_version, install_blk,
+                            present_blk, compatible_blk)
         end
+
+        private
+
+        attr_reader :install_blk, :present_blk, :compatible_blk
       end
 
       attr_reader :adapter_name, :adapter_version, :config
 
-      def initialize(adapter_name, adapter_version)
+      def initialize(adapter_name, adapter_version, install_blk, present_blk,
+                     compatible_blk)
         @adapter_name = adapter_name
         @adapter_version = adapter_version
+        @install_blk = install_blk
+        @present_blk = present_blk
+        @compatible_blk = compatible_blk
       end
 
-      # def install
-      #   if present? && compatible? && enabled?
-      #     #install...
-      #   end
-      # end
+      # Install adapter with the given config. The present? and compatbile?
+      # will be run first, and install will return false if either faile. Will
+      # return true if install was completed successfully.
+      #
+      # @param [Hash] config The config for this adapter
+      def install(config = {})
+        @config = config
+        return false unless @install_blk && present? && compatible?
+
+        instance_exec(@config, &@install_blk)
+        true
+      end
+
+      # Calls the present block of the Adapter subclasses, if no block is provided
+      # it's assumed to be present
+      def present?
+        return true unless @present_blk
+
+        instance_exec(&@present_blk)
+      end
+
+      # Calls the compatible block of the Adapter subclasses, if no block is provided
+      # it's assumed to be compatible
+      def compatible?
+        return true unless @compatible_blk
+
+        instance_exec(&@compatible_blk)
+      end
     end
   end
 end
--- api/test/opentelemetry/instrumentation/adapter_test.rb
+++ api/test/opentelemetry/instrumentation/adapter_test.rb
@@ -7,6 +7,10 @@
 require 'test_helper'
 
 describe OpenTelemetry::Instrumentation::Adapter do
+  after do
+    OpenTelemetry::Instrumentation.registry.instance_variable_set(:@adapters, [])
+  end
+
   let(:adapter) do
     Class.new(OpenTelemetry::Instrumentation::Adapter) do
       adapter_name 'test_adapter'
@@ -14,8 +18,39 @@ describe OpenTelemetry::Instrumentation::Adapter do
     end
   end
 
-  after do
-    OpenTelemetry::Instrumentation.registry.instance_variable_set(:@adapters, [])
+  let(:adapter_with_callbacks) do
+    Class.new(OpenTelemetry::Instrumentation::Adapter) do
+      attr_writer :present, :compatible
+      attr_reader :present_called, :compatible_called, :install_called,
+                  :config_yielded
+
+      adapter_name 'test_adapter'
+      adapter_version '0.1.1'
+
+      present do
+        @present_called = true
+        @present
+      end
+
+      compatible do
+        @compatible_called = true
+        @compatible
+      end
+
+      install do |config|
+        @config_yielded = config
+        @install_called = true
+      end
+
+      def initialize(*args)
+        super
+        @present = true
+        @compatible = true
+        @present_called = false
+        @compatible_called = false
+        @install_called = false
+      end
+    end
   end
 
   it 'is auto-registered' do
@@ -39,6 +74,74 @@ describe OpenTelemetry::Instrumentation::Adapter do
   describe '#adapter_name' do
     it 'returns adapter name' do
       _(adapter.instance.adapter_name).must_equal('test_adapter')
+    end
+  end
+
+  describe '#present?' do
+    describe 'with present block' do
+      it 'calls the present block' do
+        instance = adapter_with_callbacks.instance
+        _(instance.present?).must_equal(true)
+        _(instance.present_called).must_equal(true)
+      end
+    end
+
+    describe 'without present block' do
+      it 'defaults to true' do
+        instance = adapter.instance
+        _(instance.present?).must_equal(true)
+      end
+    end
+  end
+
+  describe '#compatible?' do
+    describe 'with compatible block' do
+      it 'calls the compatible block' do
+        instance = adapter_with_callbacks.instance
+        _(instance.compatible?).must_equal(true)
+        _(instance.compatible_called).must_equal(true)
+      end
+    end
+
+    describe 'without compatible block' do
+      it 'defaults to true' do
+        instance = adapter.instance
+        _(instance.compatible?).must_equal(true)
+      end
+    end
+  end
+
+  describe '#install' do
+    describe 'when installable' do
+      it 'calls the install block' do
+        instance = adapter_with_callbacks.instance
+        _(instance.install).must_equal(true)
+        _(instance.install_called).must_equal(true)
+      end
+
+      it 'yields and set config' do
+        instance = adapter_with_callbacks.instance
+        config = { option: 'value' }
+        instance.install(config)
+        _(instance.config_yielded).must_equal(config)
+        _(instance.config).must_equal(config)
+      end
+    end
+
+    describe 'when uninstallable' do
+      it 'returns false' do
+        instance = adapter_with_callbacks.instance
+        instance.compatible = false
+        _(instance.install).must_equal(false)
+        _(instance.install_called).must_equal(false)
+      end
+    end
+
+    describe 'without install block defined' do
+      it 'returns false' do
+        instance = adapter.instance
+        _(instance.install).must_equal(false)
+      end
     end
   end
 end
