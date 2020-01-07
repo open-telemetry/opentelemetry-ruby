bec8dff979876020d1fa7e8bd1a3599fb20f21cf
Matthew Wear <matthew.wear@gmail.com>
2020-01-07 11:37:29 -0800

    Document adapter

--- api/lib/opentelemetry/instrumentation/adapter.rb
+++ api/lib/opentelemetry/instrumentation/adapter.rb
@@ -9,8 +9,54 @@ module OpenTelemetry
     # The Adapter class holds all metadata and configuration for an
     # instrumentation adapter. All instrumentation adapter packages should
     # include a subclass of +Instrumentation::Adapter+ that will register
-    # it with +OpenTelemetry::Instrumentation+ and make it available for
-    # installation and configuration.
+    # it with +OpenTelemetry.instrumentation_regsitry+ and make it available for
+    # discovery and installation by an SDK.
+    #
+    # A typical subclass of Adapter will provide the adapter_name,
+    # adapter_version, an install block, a present block, and possibly
+    # a compatible block. Below is an example:
+    #
+    # module OpenTelemetry
+    #   module Adapters
+    #     module Sinatra
+    #       class Adapter < OpenTelemetry::Instrumentation::Adapter
+    #         library_name 'OpenTelemetry::Adapters::Sinatra'
+    #         library_version OpenTelemetry::Adapters::Sinatra::VERSION
+    #
+    #         install do |config|
+    #           # install instrumentation, either by library hook or applying
+    #           # a monkey patch
+    #         end
+    #
+    #         # determine if the target library is present
+    #         present do |config|
+    #           Gem.loaded_specs.key?('sinatra')
+    #         end
+    #
+    #         # if the target library is present, is it compatible?
+    #         compatible do
+    #           Gem.loaded_specs['sinatra'].version > MIN_VERSION
+    #         end
+    #       end
+    #     end
+    #   end
+    # end
+    #
+    # All subclasses of OpenTelemetry::Instrumentation::Adapter are automatically
+    # registered with OpenTelemetry.instrumentation_registry which is used by
+    # SDKs for for instrumentation discovery and installation.
+    #
+    # Instrumentation libraries can use the adapter subclass to easily gain
+    # a referene to its named tracer. For example:
+    #
+    # OpenTelemetry::Adapters::Sintatra.instance.tracer
+    #
+    # The adapter class establishes a convention for disabling an adapter
+    # by environment variable and local configuration. An adapter disabled
+    # by environment variable will take precedent over local config. The
+    # convention for enviroment variable name is the library name, upcased with
+    # '::' replaced by underscores, and '_ENABLED' appended. For example:
+    # OPENTELEMETRY_ADAPTERS_SINATRA_ENABLED = false.
     class Adapter
       class << self
         private :new # rubocop:disable Style/AccessModifierDeclarations
@@ -19,6 +65,11 @@ module OpenTelemetry
           OpenTelemetry.instrumentation_registry.register(subclass)
         end
 
+        # The name of this instrumentation adapter. Typically this is going
+        # to be the name of the instrumentation package. For example,
+        # 'OpenTelemetry::Adapters::Sinatra'
+        #
+        # @param [String] adapter_name The full name of the adapter package
         def adapter_name(adapter_name = nil)
           if adapter_name
             @adapter_name = adapter_name
@@ -27,6 +78,11 @@ module OpenTelemetry
           end
         end
 
+        # The version of this adapter. Typically this will be the package
+        # version of the adapter. For example,
+        # OpenTelemetry::Adapters::Sintra::VERSION
+        #
+        # @param [String] adapter_version The version of the adapter package
         def adapter_version(adapter_version = nil)
           if adapter_version
             @adapter_version = adapter_version
@@ -35,14 +91,32 @@ module OpenTelemetry
           end
         end
 
+        # The install block for this adapter. This will be where you install
+        # instrumentation, either by framework hook or applying a monkey patch.
+        #
+        # @param [Callable] blk The install block for this adapter
+        # @yieldparam [Hash] config The adapter config will be yielded to the
+        #   install block
         def install(&blk)
           @install_blk = blk
         end
 
+        # The present block for this adapter. This block is used to detect if
+        # target library is present on the system. Typically this will involve
+        # checking to see if the target gem spec was loaded or if expected
+        # constants from the target library are present.
+        #
+        # @param [Callable] blk The present block for this adapter
         def present(&blk)
           @present_blk = blk
         end
 
+        # The compatible block for this adapter. This check will be run if the
+        # target library is present to determine if it's compatible. It's not
+        # required, but a common use case will be to check to target library
+        # version for compatibility.
+        #
+        # @param [Callable] blk The compatibility block for this adapter
         def compatible(&blk)
           @compatible_blk = blk
         end
@@ -130,7 +204,7 @@ module OpenTelemetry
       # environment variable will be the adapter name upper cased, with '::'
       # replaced by underscores and _ENABLED appended. For example, the
       # environment variable name for OpenTelemetry::Adapter::Sinatra will be
-      # OPENTELEMETRY_ADAPTER_SINATRA_ENABLED. A value of 'false' will disable
+      # OPENTELEMETRY_ADAPTERS_SINATRA_ENABLED. A value of 'false' will disable
       # the adapter, all other values will enable it.
       def enabled_by_env_var?
         var_name = name.dup.tap do |n|
