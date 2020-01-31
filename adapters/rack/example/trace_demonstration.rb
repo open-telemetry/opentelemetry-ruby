87e74b861483c12f46677dbb0c34931ce4d24f49
David Davis <david.davis@mutations.ltd>
2020-01-30 17:39:01 -0800

    Rewrite examples
    * one that is simple, no config options
      * demonstrate integration via 'Rack::Builder#use'
      * this is how ddtrace is currently working
    
    * demonstrate integration using config[:application]
    
    * ultimate goal: 0 config (automagic integration)

--- adapters/rack/example/trace_demonstration.rb
+++ adapters/rack/example/trace_demonstration.rb
@@ -9,15 +9,18 @@ require 'bundler/setup'
 
 Bundler.require
 
-# setup fake rack application:
-builder = Rack::Builder.new
-app = ->(_env) { [200, { 'Content-Type' => 'text/plain' }, ['All responses are OK']] }
-builder.run app
-
 OpenTelemetry::SDK.configure do |c|
-  c.use 'OpenTelemetry::Adapters::Rack', retain_middleware_names: true,
-                                         application: builder,
-                                         record_frontend_span: true
+  c.use 'OpenTelemetry::Adapters::Rack'
+end
+
+# setup fake rack application:
+builder = Rack::Builder.app do
+  # integration should be automatic in web frameworks (like rails),
+  # but for a plain Rack application, enable it in your config.ru, e.g.,
+  use OpenTelemetry::Adapters::Rack::Middlewares::TracerMiddleware
+
+  app = ->(_env) { [200, { 'Content-Type' => 'text/plain' }, ['All responses are OK']] }
+  run app
 end
 
 # demonstrate tracing (span output to console):
---
+++ adapters/rack/example/trace_demonstration2.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Copyright 2020 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'rubygems'
+require 'bundler/setup'
+
+Bundler.require
+
+# setup fake rack application:
+builder = Rack::Builder.new
+app = ->(_env) { [200, { 'Content-Type' => 'text/plain' }, ['All responses are OK']] }
+builder.run app
+
+
+# demonstrate integration using config[:application] instead of 'builder.use':
+OpenTelemetry::SDK.configure do |c|
+  c.use 'OpenTelemetry::Adapters::Rack', retain_middleware_names: true,
+                                         application: builder,
+                                         record_frontend_span: true
+end
+
+# demonstrate tracing (span output to console):
+puts Rack::MockRequest.new(builder).get('/')
