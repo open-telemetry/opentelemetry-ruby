64c139839069ca78c98317acf941eb6d47fb896d
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2020-03-18 23:06:42 -0400

    Implement force_flush

--- sdk/lib/opentelemetry/sdk/trace/multi_span_processor.rb
+++ sdk/lib/opentelemetry/sdk/trace/multi_span_processor.rb
@@ -41,6 +41,17 @@ module OpenTelemetry
           @span_processors.each { |processor| processor.on_finish(span) }
         end
 
+        # Export all ended spans to the configured `Exporter` that have not yet
+        # been exported.
+        #
+        # This method should only be called in cases where it is absolutely
+        # necessary, such as when using some FaaS providers that may suspend
+        # the process after an invocation, but before the `Processor` exports
+        # the completed spans.
+        def force_flush
+          @span_processors.each &:force_flush
+        end
+
         # Called when {TracerProvider#shutdown} is called.
         def shutdown
           @span_processors.each(&:shutdown)
