40a26384df381b764e8b971b83e5b3fdb2ab1726
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2020-03-19 16:25:01 -0400

    Remove Gauges and LabelSet (#210)

--- api/lib/opentelemetry/metrics/meter.rb
+++ api/lib/opentelemetry/metrics/meter.rb
@@ -8,46 +8,9 @@ module OpenTelemetry
   module Metrics
     # No-op implementation of Meter.
     class Meter
-      NOOP_LABEL_SET = Object.new
-      private_constant(:NOOP_LABEL_SET)
+      def record_batch(*measurements, labels: nil); end
 
-      def record_batch(*measurements, label_set: nil); end
-
-      # Canonicalizes labels, returning an opaque {LabelSet} object.
-      #
-      # @param [Hash<String, String>] labels
-      # @return [LabelSet]
-      def labels(labels)
-        NOOP_LABEL_SET
-      end
-
-      # Create and return a floating point gauge.
-      #
-      # @param [String] name Name of the metric. See {Meter} for required metric name syntax.
-      # @param [optional String] description Descriptive text documenting the instrument.
-      # @param [optional String] unit Unit specified according to http://unitsofmeasure.org/ucum.html.
-      # @param [optional Enumerable<String>] recommended_label_keys Recommended grouping keys for this instrument.
-      # @param [optional Boolean] monotonic Whether the gauge accepts only monotonic updates. Defaults to false.
-      # @return [FloatGauge]
-      def create_float_gauge(name, description: nil, unit: nil, recommended_label_keys: nil, monotonic: false)
-        raise ArgumentError if name.nil?
-
-        Instruments::FloatGauge.new
-      end
-
-      # Create and return an integer gauge.
-      #
-      # @param [String] name Name of the metric. See {Meter} for required metric name syntax.
-      # @param [optional String] description Descriptive text documenting the instrument.
-      # @param [optional String] unit Unit specified according to http://unitsofmeasure.org/ucum.html.
-      # @param [optional Enumerable<String>] recommended_label_keys Recommended grouping keys for this instrument.
-      # @param [optional Boolean] monotonic Whether the gauge accepts only monotonic updates. Defaults to false.
-      # @return [IntegerGauge]
-      def create_integer_gauge(name, description: nil, unit: nil, recommended_label_keys: nil, monotonic: false)
-        raise ArgumentError if name.nil?
-
-        Instruments::IntegerGauge.new
-      end
+      # TODO: Observers.
 
       # Create and return a floating point counter.
       #
