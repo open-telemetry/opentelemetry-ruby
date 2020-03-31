68d00d3579feb3cb194dc872656b805ea9d291be
Robert Laurin <robert.laurin@shopify.com>
2020-03-31 16:26:15 -0400

    Add message.destination to server tracing middleware span

--- adapters/sidekiq/lib/opentelemetry/adapters/sidekiq/middlewares/server/tracer_middleware.rb
+++ adapters/sidekiq/lib/opentelemetry/adapters/sidekiq/middlewares/server/tracer_middleware.rb
@@ -16,6 +16,7 @@ module OpenTelemetry
                 msg['wrapped']&.to_s || msg['class'],
                 attributes: {
                   'job_id' => msg['jid'],
+                  'messaging.destination' => msg['queue'],
                   'created_at' => msg['created_at'],
                   'enqueued_at' => msg['enqueued_at'],
                 },
