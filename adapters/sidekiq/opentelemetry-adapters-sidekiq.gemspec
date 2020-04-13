34af337d01bad08e9c6994bc6977e498121b9da0
Robert Laurin <robert.laurin@shopify.com>
2020-04-13 18:03:35 -0400

    Add additional appraisail, and improve sidekiq adpater specs

--- adapters/sidekiq/opentelemetry-adapters-sidekiq.gemspec
+++ adapters/sidekiq/opentelemetry-adapters-sidekiq.gemspec
@@ -34,7 +34,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rubocop', '~> 0.73.0'
   spec.add_development_dependency 'sidekiq', '~> 5.0.0'
   spec.add_development_dependency 'simplecov', '~> 0.17.1'
-  spec.add_development_dependency 'webmock', '~> 3.7.6'
   spec.add_development_dependency 'yard', '~> 0.9'
   spec.add_development_dependency 'yard-doctest', '~> 0.1.6'
 end
--- adapters/sidekiq/test/opentelemetry/adapters/sidekiq/adapter_test.rb
+++ adapters/sidekiq/test/opentelemetry/adapters/sidekiq/adapter_test.rb
@@ -26,6 +26,9 @@ end
 describe OpenTelemetry::Adapters::Sidekiq::Adapter do
   let(:adapter) { OpenTelemetry::Adapters::Sidekiq::Adapter.instance }
   let(:exporter) { EXPORTER }
+  let(:spans) { exporter.finished_spans }
+  let(:root_span) { spans.find { |s| s.parent_span_id == '0000000000000000' } }
+
   before { exporter.reset }
 
   describe 'tracing' do
@@ -38,15 +41,46 @@ describe OpenTelemetry::Adapters::Sidekiq::Adapter do
     end
 
     it 'after performing a simple job' do
-      Sidekiq::Testing.inline! { SimpleJob.perform_async }
-
+      job_id = Sidekiq::Testing.inline! { SimpleJob.perform_async }
       _(exporter.finished_spans.size).must_equal 2
+
+      _(root_span.attributes['job_id']).must_equal job_id
+      _(root_span.attributes['messaging.destination']).must_equal 'default'
+      _(root_span.attributes['created_at']).wont_be_nil
+      _(root_span.name).must_equal 'SimpleJob'
+      _(root_span.kind).must_equal :producer
+      _(root_span.parent_span_id).must_equal '0000000000000000'
+
+      child_span = exporter.finished_spans.last
+      _(child_span.attributes['job_id']).must_equal job_id
+      _(child_span.attributes['messaging.destination']).must_equal 'default'
+      _(child_span.attributes['created_at']).wont_be_nil
+      _(child_span.name).must_equal 'SimpleJob'
+      _(child_span.kind).must_equal :consumer
+      _(child_span.parent_span_id).must_equal root_span.span_id
+
+      _(child_span.trace_id).must_equal root_span.trace_id
     end
 
     it 'after performing a simple job enqueuer' do
-      Sidekiq::Testing.inline! { SimpleEnqueueingJob.perform_async }
-
+      job_id = Sidekiq::Testing.inline! { SimpleEnqueueingJob.perform_async }
       _(exporter.finished_spans.size).must_equal 4
+
+      _(root_span.parent_span_id).must_equal '0000000000000000'
+      _(root_span.name).must_equal 'SimpleEnqueueingJob'
+      _(root_span.kind).must_equal :producer
+
+      child_span_1 = spans.find { |s| s.parent_span_id == root_span.span_id }
+      _(child_span_1.name).must_equal 'SimpleEnqueueingJob'
+      _(child_span_1.kind).must_equal :consumer
+
+      child_span_2 = spans.find { |s| s.parent_span_id == child_span_1.span_id }
+      _(child_span_2.name).must_equal 'SimpleJob'
+      _(child_span_2.kind).must_equal :producer
+
+      child_span_3 = spans.find { |s| s.parent_span_id == child_span_2.span_id }
+      _(child_span_3.name).must_equal 'SimpleJob'
+      _(child_span_3.kind).must_equal :consumer
     end
   end
 end
