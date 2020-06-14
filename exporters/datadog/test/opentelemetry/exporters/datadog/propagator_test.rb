4921e9ac90eec2b7c4f465d215f2f909d11d90f2
ericmustin <mustin.eric@gmail.com>
2020-06-15 00:59:01 +0200

    [contrib-datadog]: add skeleton tests for injection, kick jruby build

--- exporters/datadog/test/opentelemetry/exporters/datadog/propagator_test.rb
+++ exporters/datadog/test/opentelemetry/exporters/datadog/propagator_test.rb
@@ -6,6 +6,11 @@
 
 describe OpenTelemetry::Exporters::Datadog::Exporter do
   describe '#inject' do
+    it 'yields the carrier' do
+    end
+
+    it 'injects the datadog appropriate trace information into the carrier from the context, if provided' do
+    end
   end
 
   describe '#extract' do
@@ -13,6 +18,9 @@ describe OpenTelemetry::Exporters::Datadog::Exporter do
     end
 
     it 'returns a remote SpanContext with fields from the datadog headers' do
+    end
+
+    it 'accounts for rack specific headers' do
     end
   end
 end
