b2436a12ceafcaabfbd742cc2df4e82582341dcc
ericmustin <mustin.eric@gmail.com>
2020-06-23 13:36:57 +0200

    fix span naming code

--- exporters/datadog/lib/opentelemetry/exporters/datadog/exporter/span_encoder.rb
+++ exporters/datadog/lib/opentelemetry/exporters/datadog/exporter/span_encoder.rb
@@ -172,7 +172,7 @@ module OpenTelemetry
             # Get span name by using instrumentation and kind while backing off to
             instrumentation_name = span.instrumentation_library&.name
             kind = span.kind
-            instrumentation_name && kind ? instrumentation_name.kind.to_s.gsub(':', '_') : span.name
+            instrumentation_name && kind ? "#{instrumentation_name.to_s.gsub(':', '_')}.#{kind}" : span.name
           end
 
           def get_origin_string(tracestate)
