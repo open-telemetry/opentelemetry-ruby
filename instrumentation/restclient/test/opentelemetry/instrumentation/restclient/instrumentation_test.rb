97af67b558bb0e72151f6d3461e68d5b6323c8cc
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2020-08-24 18:58:46 -0400

    Add hex_trace_id and hex_span_id helpers (#332)
    * Add hex_trace_id and hex_span_id helpers
    
    * Rubocop
    
    * Silly error

--- instrumentation/restclient/test/opentelemetry/instrumentation/restclient/instrumentation_test.rb
+++ instrumentation/restclient/test/opentelemetry/instrumentation/restclient/instrumentation_test.rb
@@ -55,7 +55,7 @@ describe OpenTelemetry::Instrumentation::RestClient::Instrumentation do
       assert_requested(
         :get,
         'http://example.com/success',
-        headers: { 'Traceparent' => "00-#{span.trace_id.unpack1('H*')}-#{span.span_id.unpack1('H*')}-01" }
+        headers: { 'Traceparent' => "00-#{span.hex_trace_id}-#{span.hex_span_id}-01" }
       )
     end
 
@@ -72,7 +72,7 @@ describe OpenTelemetry::Instrumentation::RestClient::Instrumentation do
       assert_requested(
         :get,
         'http://example.com/failure',
-        headers: { 'Traceparent' => "00-#{span.trace_id.unpack1('H*')}-#{span.span_id.unpack1('H*')}-01" }
+        headers: { 'Traceparent' => "00-#{span.hex_trace_id}-#{span.hex_span_id}-01" }
       )
     end
   end
--- sdk/lib/opentelemetry/sdk/trace/span_data.rb
+++ sdk/lib/opentelemetry/sdk/trace/span_data.rb
@@ -28,7 +28,21 @@ module OpenTelemetry
                             :span_id,
                             :trace_id,
                             :trace_flags,
-                            :tracestate)
+                            :tracestate) do
+                              # Returns the lowercase [hex encoded](https://tools.ietf.org/html/rfc4648#section-8) span ID.
+                              #
+                              # @return [String] A 16-hex-character lowercase string.
+                              def hex_span_id
+                                span_id.unpack1('H*')
+                              end
+
+                              # Returns the lowercase [hex encoded](https://tools.ietf.org/html/rfc4648#section-8) trace ID.
+                              #
+                              # @return [String] A 32-hex-character lowercase string.
+                              def hex_trace_id
+                                trace_id.unpack1('H*')
+                              end
+                            end
     end
   end
 end
