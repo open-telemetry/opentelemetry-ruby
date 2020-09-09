1bff7c9a24802b33f2ed36814a7461b3705e5599
Robert <robertlaurin@users.noreply.github.com>
2020-09-09 08:31:00 -0500

    feat: Add Dalli instrumentation gem (#355)
    * Add Dalli instrumentation gem
    
    * Rubocop auto fixes
    
    * Rubocop auto fixes
    
    * Add Dalli to releases.yml

---
+++ instrumentation/dalli/lib/opentelemetry/instrumentation/dalli/patches/server.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# Copyright 2020 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+module OpenTelemetry
+  module Instrumentation
+    module Dalli
+      module Patches
+        # Module to prepend to Dalli::Server for instrumentation
+        module Server
+          def request(op, *args)
+            operation = Utils.opname(op, multi?)
+            attributes = {
+              'db.system' => 'memcached',
+              'db.statement' => Utils.format_command(operation, args),
+              'net.peer.name' => hostname,
+              'net.peer.port' => port
+            }
+
+            tracer.in_span(operation, attributes: attributes, kind: :client) do
+              super
+            end
+          end
+
+          private
+
+          def tracer
+            Dalli::Instrumentation.instance.tracer
+          end
+        end
+      end
+    end
+  end
+end
