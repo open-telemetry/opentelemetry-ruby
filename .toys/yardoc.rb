0d1b7600fd0fa185d16bd2961172894bcb7baf92
Daniel Azuma <dazuma@google.com>
2020-09-22 04:39:41 +0000

    chore: Tool for building yardoc during release

---
+++ .toys/yardoc.rb
@@ -0,0 +1,15 @@
+desc "Build YARD documentation for the gem in the current directory"
+
+include :exec, e: true
+include :fileutils
+
+set_context_directory Dir.pwd
+
+def run
+  raise "No yardopts in the current directory" unless File.file?(".yardopts")
+  rm_rf(".yardoc")
+  rm_rf("doc")
+  exec_tool(["yardoc", "_build"])
+end
+
+expand :yardoc, name: "_build", bundler: true
