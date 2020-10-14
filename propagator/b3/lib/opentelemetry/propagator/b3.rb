e07439e53bddfeb8cbfcfc5687e75d3eb3fcdaeb
Matthew Wear <matthew.wear@gmail.com>
2020-10-14 15:54:51 -0700

    chore: boilerplate for b3 gem

---
+++ propagator/b3/lib/opentelemetry/propagator/b3.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Copyright 2020 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# OpenTelemetry is an open source observability framework, providing a
+# general-purpose API, SDK, and related tools required for the instrumentation
+# of cloud-native software, frameworks, and libraries.
+#
+# The OpenTelemetry module provides global accessors for telemetry objects.
+# See the documentation for the `opentelemetry-api` gem for details.
+module OpenTelemetry
+  module Propagator
+    # Namespace for OpenTelemetry propagator extension libraries
+    module B3
+    end
+  end
+end
+
+require_relative './b3/version'
