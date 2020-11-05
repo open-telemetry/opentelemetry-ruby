9a7c582f1a6fc9118b6fe4d13ceb84ebf2bf90c4
johnnyshields <johnny.shields@gmail.com>
2020-11-06 00:29:04 +0900

    feat: add OTEL_START_EXPORT_THREAD_ON_BOOT env var to control lazy thread spawn behavior.
    fixes #463

--- sdk/lib/opentelemetry/sdk/trace/export/batch_span_processor.rb
+++ sdk/lib/opentelemetry/sdk/trace/export/batch_span_processor.rb
@@ -47,7 +47,8 @@ module OpenTelemetry
                          exporter_timeout_millis: Float(ENV.fetch('OTEL_BSP_EXPORT_TIMEOUT_MILLIS', 30_000)),
                          schedule_delay_millis: Float(ENV.fetch('OTEL_BSP_SCHEDULE_DELAY_MILLIS', 5_000)),
                          max_queue_size: Integer(ENV.fetch('OTEL_BSP_MAX_QUEUE_SIZE', 2048)),
-                         max_export_batch_size: Integer(ENV.fetch('OTEL_BSP_MAX_EXPORT_BATCH_SIZE', 512)))
+                         max_export_batch_size: Integer(ENV.fetch('OTEL_BSP_MAX_EXPORT_BATCH_SIZE', 512)),
+                         start_thread_on_boot: OpenTelemetry::SDK::Internal.to_boolean(ENV['OTEL_START_EXPORT_THREAD_ON_BOOT']))
             raise ArgumentError if max_export_batch_size > max_queue_size
 
             @exporter = exporter
@@ -61,7 +62,8 @@ module OpenTelemetry
             @spans = []
             @pid = nil
             @thread = nil
-            reset_on_fork
+            start_thread_on_boot = true if start_thread_on_boot.nil?
+            reset_on_fork(restart_thread: start_thread_on_boot)
           end
 
           # does nothing for this processor
