a0c8629a5e9a242c830353e9701b9e188f977b12
Johnny Shields <johnny.shields@gmail.com>
2020-11-19 22:55:23 +0900

    feat: Delayed Job instrumentation (ported from Datadog) (#446)
    * feat: Delayed Job instrumentation (ported from Datadog)
    
    * fix: bump gem versions
    
    * fix: make delayed_job instrumentation conform to "messaging" standard
    
    * fix: remove unneeded rubocop disable
    
    * fix: remove component attribute
    
    * Apply suggestions from code review
    
    Co-authored-by: Francis Bogsanyi <francis.bogsanyi@shopify.com>
    
    * fix: test
    
    * fix: requested changes
    
    * fix: ok, rubocop...
    
    * fix: Remove commented code related to shutdown
    
    * fix: Remove commented code related to shutdown
    
    * chore: rename "TracerMiddleware" to "TracerPlugin"
    
    * fix: use Plugins namespace
    
    Co-authored-by: Francis Bogsanyi <francis.bogsanyi@shopify.com>

---
+++ instrumentation/delayed_job/lib/opentelemetry/instrumentation/delayed_job/instrumentation.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# Copyright 2019 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+module OpenTelemetry
+  module Instrumentation
+    module DelayedJob
+      # Instrumentation class that detects and installs the DelayedJob instrumentation
+      class Instrumentation < OpenTelemetry::Instrumentation::Base
+        MINIMUM_VERSION = Gem::Version.new('4.1')
+
+        install do |_config|
+          require_dependencies
+          register_tracer_plugin
+        end
+
+        present do
+          !defined?(::Delayed).nil? && gem_version >= MINIMUM_VERSION
+        end
+
+        private
+
+        def gem_version
+          Gem.loaded_specs['delayed_job'].version
+        end
+
+        def require_dependencies
+          require_relative 'plugins/tracer_plugin'
+        end
+
+        def register_tracer_plugin
+          ::Delayed::Worker.plugins << Plugins::TracerPlugin
+        end
+      end
+    end
+  end
+end
