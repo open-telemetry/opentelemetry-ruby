e8b80dac95324cda3bb908de26afae2e26f8b35b
Robert <robertlaurin@users.noreply.github.com>
2020-12-10 21:43:17 -0500

    feat: move utf8 encoding to common utils (#511)

--- instrumentation/redis/test/opentelemetry/instrumentation/redis/utils_test.rb
+++ instrumentation/redis/test/opentelemetry/instrumentation/redis/utils_test.rb
@@ -86,37 +86,4 @@ describe OpenTelemetry::Instrumentation::Redis::Utils do
       it { _(subject).must_equal('SET KEY VALUE') }
     end
   end
-
-  describe '#utf8_encode' do
-    it 'happy path' do
-      str = 'pristine ￢'.encode(Encoding::UTF_8)
-
-      assert_equal('pristine ￢', utils.utf8_encode(str))
-
-      assert_equal(::Encoding::UTF_8, utils.utf8_encode(str).encoding)
-
-      # we don't allocate new objects when a valid UTF-8 string is provided
-      assert_same(str, utils.utf8_encode(str))
-    end
-
-    it 'with invalid conversion' do
-      time_bomb = "\xC2".dup.force_encoding(::Encoding::ASCII_8BIT)
-
-      # making sure this is indeed a problem
-      assert_raises(Encoding::UndefinedConversionError) do
-        time_bomb.encode(Encoding::UTF_8)
-      end
-
-      assert_equal(utils::STRING_PLACEHOLDER, utils.utf8_encode(time_bomb))
-
-      # we can also set a custom placeholder
-      assert_equal('?', utils.utf8_encode(time_bomb, placeholder: '?'))
-    end
-
-    it 'with binardy data' do
-      byte_array = "keep what\xC2 is valid".dup.force_encoding(::Encoding::ASCII_8BIT)
-
-      assert_equal('keep what is valid', utils.utf8_encode(byte_array, binary: true))
-    end
-  end
 end
