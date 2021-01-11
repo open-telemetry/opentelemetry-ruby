9fd413099d4f08fd40dbd7a9c08a780e4229868e
johnnyshields <johnny.shields@gmail.com>
2021-01-12 01:27:42 +0900

    Add minimum gem version for ruby-kafka

--- instrumentation/ruby_kafka/opentelemetry-instrumentation-ruby_kafka.gemspec
+++ instrumentation/ruby_kafka/opentelemetry-instrumentation-ruby_kafka.gemspec
@@ -31,6 +31,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '>= 1.17'
   spec.add_development_dependency 'minitest', '~> 5.0'
   spec.add_development_dependency 'opentelemetry-sdk', '~> 0.0'
+  spec.add_development_dependency 'rspec-mocks'
   spec.add_development_dependency 'rubocop', '~> 0.73.0'
   spec.add_development_dependency 'ruby-kafka'
   spec.add_development_dependency 'simplecov', '~> 0.17.1'
--- instrumentation/ruby_kafka/test/opentelemetry/instrumentation/ruby-kafka/instrumentation_test.rb
+++ instrumentation/ruby_kafka/test/opentelemetry/instrumentation/ruby-kafka/instrumentation_test.rb
@@ -20,6 +20,29 @@ describe OpenTelemetry::Instrumentation::RubyKafka::Instrumentation do
     _(instrumentation.version).wont_be_empty
   end
 
+  describe 'present' do
+    it 'when ruby-kafka gem installed' do
+      _(instrumentation.present?).must_equal true
+    end
+
+    it 'when ruby-kafka gem not installed' do
+      hide_const('Kafka')
+      _(instrumentation.present?).must_equal false
+    end
+
+    it 'when older gem version installed' do
+      Gem.stub(:loaded_specs, 'ruby-kafka' => Gem::Specification.new { |s| s.version = '0.6.8' }) do
+        _(instrumentation.present?).must_equal false
+      end
+    end
+
+    it 'when future gem version installed' do
+      Gem.stub(:loaded_specs, 'ruby-kafka' => Gem::Specification.new { |s| s.version = '1.7.0' }) do
+        _(instrumentation.present?).must_equal true
+      end
+    end
+  end
+
   describe '#install' do
     it 'accepts arguments' do
       instrumentation.instance_variable_set(:@installed, false)
