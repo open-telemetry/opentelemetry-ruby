a1b85042d05be9bd505740f4e167bcf0997bd01c
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2021-02-05 10:44:21 -0500

    doc: remove comment referencing MultiSpanExporter

--- sdk/lib/opentelemetry/sdk/trace/tracer_provider.rb
+++ sdk/lib/opentelemetry/sdk/trace/tracer_provider.rb
@@ -65,11 +65,6 @@ module OpenTelemetry
 
         # Adds a new SpanProcessor to this {Tracer}.
         #
-        # Any registered processor causes overhead, consider to use an
-        # async/batch processor especially for span exporting, and export to
-        # multiple backends using the
-        # {io.opentelemetry.sdk.trace.export.MultiSpanExporter}.
-        #
         # @param span_processor the new SpanProcessor to be added.
         def add_span_processor(span_processor)
           @mutex.synchronize do
