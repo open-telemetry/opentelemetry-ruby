bb04fb0b80222af9cf19a5205e22e0736eb07edb
Rogerio Angeliski <rogerio.angeliski@resultadosdigitais.com.br>
2021-03-18 15:46:29 -0300

    feat: added enable_recognize_route option

--- instrumentation/rails/lib/opentelemetry/instrumentation/rails/patches/action_controller/metal.rb
+++ instrumentation/rails/lib/opentelemetry/instrumentation/rails/patches/action_controller/metal.rb
@@ -15,10 +15,20 @@ module OpenTelemetry
               rack_span = OpenTelemetry::Instrumentation::Rack.current_span
               rack_span.name = "#{self.class.name}##{name}" if rack_span.context.valid? && !request.env['action_dispatch.exception']
 
+              add_rails_route(rack_span, request) if instrumentation_config[:enable_recognize_route]
+              super(name, request, response)
+            end
+
+            private
+
+            def add_rails_route(rack_span, request)
               ::Rails.application.routes.router.recognize(request) do |route, _params|
                 rack_span.set_attribute('http.route', route.path.spec.to_s)
               end
-              super(name, request, response)
+            end
+
+            def instrumentation_config
+              Rails::Instrumentation.instance.config
             end
           end
         end
