7a0eeb5f5903cbc96a6619fa271eb3a88c2672e1
Tim Borkhodoev <38668303+SomalianIvan@users.noreply.github.com>
2021-04-26 18:34:24 -0400

    Update instrumentation/koala/lib/opentelemetry/instrumentation/koala/patches/instrumentation.rb
    Co-authored-by: Francis Bogsanyi <francis.bogsanyi@shopify.com>

--- instrumentation/koala/lib/opentelemetry/instrumentation/koala/patches/instrumentation.rb
+++ instrumentation/koala/lib/opentelemetry/instrumentation/koala/patches/instrumentation.rb
@@ -10,8 +10,6 @@ module OpenTelemetry
       module Patches
         # Module to prepend to Koala::Facebook::API for instrumentation
         module Api
-          VERBS_TO_SPAN_NAMES = Hash.new { |h, k| h[k] = "Koala #{k}" }
-
           def graph_call(path, args = {}, verb = 'get', options = {}, &post_processing)
             OpenTelemetry::Common::HTTP::ClientContext.with_attributes('peer.service' => 'facebook', 'koala.verb' => verb, 'koala.path' => path) do
               super
