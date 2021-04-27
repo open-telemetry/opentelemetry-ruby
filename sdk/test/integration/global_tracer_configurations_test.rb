475bdbdcca92206741076ae255fcc5502a95b078
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2021-04-27 17:21:28 -0400

    fix: silly mistakes

--- sdk/test/integration/global_tracer_configurations_test.rb
+++ sdk/test/integration/global_tracer_configurations_test.rb
@@ -20,7 +20,7 @@ describe OpenTelemetry::SDK, 'global_tracer_configurations' do
   let(:finished_spans) { exporter.finished_spans }
 
   before do
-    tracer.with_span(tracer.start_span('root', with_parent: parent_context)) do
+    OpenTelemetry::Trace.with_span(tracer.start_span('root', with_parent: parent_context)) do
       tracer.in_span('child1') {}
       tracer.in_span('child2') {}
     end
