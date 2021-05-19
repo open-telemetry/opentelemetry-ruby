4c633761873d5c927f1aab276eb5c04130dff278
Robert <robertlaurin@users.noreply.github.com>
2021-05-19 15:14:22 -0500

    refactor: simplify redis attribute assignment (#758)

--- instrumentation/redis/lib/opentelemetry/instrumentation/redis/patches/client.rb
+++ instrumentation/redis/lib/opentelemetry/instrumentation/redis/patches/client.rb
@@ -10,30 +10,7 @@ module OpenTelemetry
       module Patches
         # Module to prepend to Redis::Client for instrumentation
         module Client
-          def process(commands)
-            reply = nil
-            attributes = client_attributes
-            attributes['db.statement'] = Utils.format_statements(commands)
-            tracer.in_span(
-              Utils.format_span_name(commands),
-              attributes: attributes,
-              kind: :client
-            ) do |s|
-              reply = super(commands)
-              if reply.is_a?(::Redis::CommandError)
-                s.record_exception(reply)
-                s.status = Trace::Status.new(
-                  Trace::Status::ERROR,
-                  description: reply.message
-                )
-              end
-            end
-            reply
-          end
-
-          private
-
-          def client_attributes
+          def process(commands) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
             host = options[:host]
             port = options[:port]
 
@@ -42,10 +19,26 @@ module OpenTelemetry
               'net.peer.name' => host,
               'net.peer.port' => port
             }
+
             attributes['db.redis.database_index'] = options[:db] unless options[:db].zero?
             attributes['peer.service'] = config[:peer_service] if config[:peer_service]
-            attributes.merge(OpenTelemetry::Instrumentation::Redis.attributes)
+            attributes.merge!(OpenTelemetry::Instrumentation::Redis.attributes)
+            attributes['db.statement'] = Utils.format_statements(commands)
+
+            tracer.in_span(Utils.format_span_name(commands), attributes: attributes, kind: :client) do |s|
+              super(commands).tap do |reply|
+                if reply.is_a?(::Redis::CommandError)
+                  s.record_exception(reply)
+                  s.status = Trace::Status.new(
+                    Trace::Status::ERROR,
+                    description: reply.message
+                  )
+                end
+              end
+            end
           end
+
+          private
 
           def tracer
             Redis::Instrumentation.instance.tracer
