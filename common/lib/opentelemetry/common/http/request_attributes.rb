a157ca266d83c60f91770e2001be7fe70fd265df
Christopher Holmes <christopher.holmes@simplybusiness.co.uk>
2021-05-23 22:25:30 +0100

    Create a common util for http attributes
    Instrumentation for HTTP clients that use this utility will now add
     consistent attributes for HTTP requests.
    
    The utility also has an option to hide query parameters so that they
    don't show up in these attributes. Query parameters often hold sensitive
    information, which some teams may not want to log.

---
+++ common/lib/opentelemetry/common/http/request_attributes.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Copyright The OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+module OpenTelemetry
+  module Common
+    module HTTP
+      # RequestAttributes contains common helpers for setting http client attributes in spans
+      module RequestAttributes
+        extend self
+
+        def from_request(request_method, uri, config = {})
+          uri = hide_query_params(uri) if config[:hide_query_params]
+
+          {
+            'http.method' => request_method,
+            'http.scheme' => uri.scheme,
+            'http.target' => uri.request_uri,
+            'http.url' => uri.to_s,
+            'peer.hostname' => uri.host,
+            'peer.port' => uri.port
+          }.compact
+        end
+
+        def hide_query_params(uri)
+          uri.query = '' if uri.query
+
+          uri
+        end
+      end
+    end
+  end
+end
