a9347a79a3007c4cffe7f0d83c7a7c08140f4d10
Daniel Azuma <dazuma@gmail.com>
2021-06-29 21:51:41 +0000

    release: Release 2 gems

--- exporter/otlp/lib/opentelemetry/exporter/otlp/version.rb
+++ exporter/otlp/lib/opentelemetry/exporter/otlp/version.rb
@@ -8,7 +8,7 @@ module OpenTelemetry
   module Exporter
     module OTLP
       ## Current OpenTelemetry OTLP exporter version
-      VERSION = '0.20.0'
+      VERSION = '0.20.1'
     end
   end
 end
