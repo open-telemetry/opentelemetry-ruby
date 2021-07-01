82e918bc143cbda0fc6d03a5199c13af0d064f5c
Andrew Hayworth <ahayworth@gmail.com>
2021-07-01 16:55:53 -0500

    feat: Add auto-generated semantic conventions gem
    Generated from the YAML, complete with Rake task to build it.

---
+++ semantic_conventions/lib/opentelemetry/semantic_conventions.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Copyright The OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+module OpenTelemetry
+  module SemanticConventions
+  end
+end
+
+require_relative './semantic_conventions/trace'
+require_relative './semantic_conventions/resource'
