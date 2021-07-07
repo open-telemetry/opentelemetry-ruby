6d245926ee7fab6390bcdb79256101c7d2d3654e
Robert Laurin <robert.laurin@shopify.com>
2021-07-07 10:21:21 -0500

    fix: install active record in rails instrumentation

--- instrumentation/rails/lib/opentelemetry/instrumentation/rails/railtie.rb
+++ instrumentation/rails/lib/opentelemetry/instrumentation/rails/railtie.rb
@@ -11,8 +11,10 @@ module OpenTelemetry
       # instrumentation, while this Railtie is used to conventionally instrument
       # the Rails application through its initialization hooks
       class Railtie < ::Rails::Railtie
-        config.before_configuration do |app|
+        config.before_initialize do |app|
+          OpenTelemetry::Instrumentation::ActiveRecord::Instrumentation.instance.install({})
           OpenTelemetry::Instrumentation::Rack::Instrumentation.instance.install({})
+
           app.middleware.insert_before(
             0,
             OpenTelemetry::Instrumentation::Rack::Middlewares::TracerMiddleware
