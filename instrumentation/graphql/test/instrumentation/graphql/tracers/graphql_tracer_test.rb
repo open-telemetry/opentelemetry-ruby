f095738238ce70ffa6d1598da2e7f6a433f7a701
Timur Borkhodoev <tim.borkhodoev@shopify.com>
2021-08-10 09:28:12 -0400

    wip: fix tests

--- instrumentation/graphql/test/instrumentation/graphql/tracers/graphql_tracer_test.rb
+++ instrumentation/graphql/test/instrumentation/graphql/tracers/graphql_tracer_test.rb
@@ -166,7 +166,7 @@ describe OpenTelemetry::Instrumentation::GraphQL::Tracers::GraphQLTracer do
       span = spans.find { |s| s.name == 'graphql.validate' }
       event = span.events.find { |e| e.name == 'graphql.validation.error' }
       _(event.attributes['message']).must_equal(
-        ["{\"message\":\"Field 'nonExistentField' doesn't exist on type 'Query'\",\"locations\":[{\"line\":2,\"column\":13}],\"path\":[\"query\",\"nonExistentField\"],\"extensions\":{\"code\":\"undefinedField\",\"typeName\":\"Query\",\"fieldName\":\"nonExistentField\"}}"]
+        "[{\"message\":\"Field 'nonExistentField' doesn't exist on type 'Query'\",\"locations\":[{\"line\":2,\"column\":13}],\"path\":[\"query\",\"nonExistentField\"],\"extensions\":{\"code\":\"undefinedField\",\"typeName\":\"Query\",\"fieldName\":\"nonExistentField\"}}]"
       )
     end
   end
