b052e46f271bd9b6c7d727db6c26b88352784239
Amir Blum <amir@aspecto.io>
2021-11-18 10:29:58 +0200

    refactor: rename helper env fucntion to be uniform

--- resource_detectors/lib/opentelemetry/resource/detectors/deployment.rb
+++ resource_detectors/lib/opentelemetry/resource/detectors/deployment.rb
@@ -9,14 +9,14 @@ module OpenTelemetry
 
         def detect
           resource_attributes = {}
-          deployment_environment = rails_deployment_environment || rack_env
+          deployment_environment = rails_env || rack_env
           resource_attributes[::OpenTelemetry::SemanticConventions::Resource::DEPLOYMENT_ENVIRONMENT] = deployment_environment.to_str if deployment_environment
           OpenTelemetry::SDK::Resources::Resource.create(resource_attributes)
         end
 
         private
 
-        def rails_deployment_environment
+        def rails_env
           # rails extract env like this:
           # https://github.com/rails/rails/blob/5647a9c1ced68d20338552d47a3b755e10a271c4/railties/lib/rails.rb#L74
           # ActiveSupport::EnvironmentInquirer.new(ENV["RAILS_ENV"].presence || ENV["RACK_ENV"].presence || "development")
