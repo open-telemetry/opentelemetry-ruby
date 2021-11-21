b23ede3351db29175c84ea5569bfe888bf6c63a9
Yaniv Davidi <yaniv@aspecto.io>
2021-11-21 17:14:43 +0200

    cover MessageAttributeSetter with tests

--- instrumentation/aws_sdk/test/opentelemetry/instrumentation_test.rb
+++ instrumentation/aws_sdk/test/opentelemetry/instrumentation_test.rb
@@ -160,4 +160,42 @@ describe OpenTelemetry::Instrumentation::AwsSdk do
       end
     end
   end
+
+  describe 'MessageAttributeSetter' do
+    it 'set when hash length is lower than 10' do
+      key = 'foo'
+      value = 'bar'
+      metadata_attributes = {}
+      OpenTelemetry::Instrumentation::AwsSdk::MessageAttributeSetter.set(metadata_attributes, key, value)
+      _(metadata_attributes[key]).must_equal(string_value: value, data_type: 'String')
+    end
+
+    it 'should keep existing attributes' do
+      key = 'foo'
+      value = 'bar'
+      metadata_attributes = {
+        'existingKey' => { string_value: 'existingValue', data_type: 'String' }
+      }
+      OpenTelemetry::Instrumentation::AwsSdk::MessageAttributeSetter.set(metadata_attributes, key, value)
+      _(metadata_attributes[key]).must_equal(string_value: value, data_type: 'String')
+      _(metadata_attributes['existingKey']).must_equal(string_value: 'existingValue', data_type: 'String')
+    end
+
+    it 'should not add if there are 10 or more existing attributes' do
+      metadata_attributes = {
+        'existingKey0' => { string_value: 'existingValue', data_type: 'String' },
+        'existingKey1' => { string_value: 'existingValue', data_type: 'String' },
+        'existingKey2' => { string_value: 'existingValue', data_type: 'String' },
+        'existingKey3' => { string_value: 'existingValue', data_type: 'String' },
+        'existingKey4' => { string_value: 'existingValue', data_type: 'String' },
+        'existingKey5' => { string_value: 'existingValue', data_type: 'String' },
+        'existingKey6' => { string_value: 'existingValue', data_type: 'String' },
+        'existingKey7' => { string_value: 'existingValue', data_type: 'String' },
+        'existingKey8' => { string_value: 'existingValue', data_type: 'String' },
+        'existingKey9' => { string_value: 'existingValue', data_type: 'String' }
+      }
+      OpenTelemetry::Instrumentation::AwsSdk::MessageAttributeSetter.set(metadata_attributes, 'new10', 'value')
+      _(metadata_attributes.keys).must_equal(%w[existingKey0 existingKey1 existingKey2 existingKey3 existingKey4 existingKey5 existingKey6 existingKey7 existingKey8 existingKey9])
+    end
+  end
 end
