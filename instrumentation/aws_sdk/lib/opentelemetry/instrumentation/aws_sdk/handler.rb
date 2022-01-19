658440f2f749f151fde393ce1654803a816f1c38
Yaniv Davidi <yaniv@aspecto.io>
2022-01-19 18:50:18 +0200

    feat: SQS / SNS messaging attributes and context propagation (#1026)
    * fix: aws-sdk nitpicks
    
    * implement sqs/sns context propagation
    
    * apply message attributes for SQS/SNS
    
    * using client_method instead of operation
    
    * fix context propagation for sqs send batch
    
    * add tests for sns & sqs services
    
    * cover MessageAttributeSetter with tests
    
    * aws-sdk test - ignore OpenTelemetry logs
    
    * rubocop fix
    
    * enable github actions on fork repo
    
    * support context data structure for older versions
    
    * Revert "enable github actions on fork repo"
    
    This reverts commit 71dd2689bf9e686ccc96886dc466113957a918e6.
    
    * fix aws-sdk status message in case of an error
    
    * remove unnecessary rubocop disable
    
    * fix support older aws-sdk versions
    
    * enable ci in fork repo
    
    * remove forgotten comments
    
    * use class to_s instead of name
    
    * fix rubocop issue
    
    * remove unused param
    
    * Revert "enable ci in fork repo"
    
    This reverts commit c357c676575250dd6d4179596c70eb68259617a7.
    
    * extract operation only from `context.operation&.name`
    
    * get context params always from 'context.params'
    
    * fix span_kind
    
    * Trigger EasyCLA
    
    * use 'phone_number' instead of real phone number
    
    * uninstrument v2.0
    
    * add config inject_messaging_context
    
    * add MessageAttributeGetter example
    
    * re-add support for aws-sdk v2.0.x
    
    * ignore rubocop RescueModifier issue
    
    * Update instrumentation/aws_sdk/lib/opentelemetry/instrumentation/aws_sdk/messaging_helper.rb
    
    Co-authored-by: Matthew Wear <matthew.wear@gmail.com>
    
    * fix queue_name method
    
    * use private const SEND_MESSAGE_CLIENT_METHODS
    
    * chore(aws_sdk): improve getting queue name
    
    * chore(aws-sdk): improve service_name extraction
    
    * ignore rubocop error
    
    Co-authored-by: Robert Laurin <robert.laurin@shopify.com>
    Co-authored-by: Matthew Wear <matthew.wear@gmail.com>
    Co-authored-by: Francis Bogsanyi <francis.bogsanyi@shopify.com>

--- instrumentation/aws_sdk/lib/opentelemetry/instrumentation/aws_sdk/handler.rb
+++ instrumentation/aws_sdk/lib/opentelemetry/instrumentation/aws_sdk/handler.rb
@@ -9,11 +9,17 @@ module OpenTelemetry
     module AwsSdk
       # Generates Spans for all interactions with AwsSdk
       class Handler < Seahorse::Client::Handler
-        def call(context) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+        SQS_SEND_MESSAGE = 'SQS.SendMessage'
+        SQS_SEND_MESSAGE_BATCH = 'SQS.SendMessageBatch'
+        SQS_RECEIVE_MESSAGE = 'SQS.ReceiveMessage'
+        SNS_PUBLISH = 'SNS.Publish'
+
+        def call(context) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
           return super unless context
 
-          service_name = context.client.class.api.metadata['serviceId'] || context.client.class.to_s.split('::')[1]
+          service_name = service_name(context)
           operation = context.operation&.name
+          client_method = "#{service_name}.#{operation}"
           attributes = {
             'aws.region' => context.config.region,
             OpenTelemetry::SemanticConventions::Trace::RPC_SYSTEM => 'aws-api',
@@ -21,8 +27,11 @@ module OpenTelemetry
             OpenTelemetry::SemanticConventions::Trace::RPC_SERVICE => service_name
           }
           attributes[SemanticConventions::Trace::DB_SYSTEM] = 'dynamodb' if service_name == 'DynamoDB'
+          MessagingHelper.apply_sqs_attributes(attributes, context, client_method) if service_name == 'SQS'
+          MessagingHelper.apply_sns_attributes(attributes, context, client_method) if service_name == 'SNS'
 
-          tracer.in_span("#{service_name}.#{operation}", attributes: attributes, kind: OpenTelemetry::Trace::SpanKind::CLIENT) do |span|
+          tracer.in_span(span_name(context, client_method), attributes: attributes, kind: span_kind(client_method)) do |span|
+            inject_context(context, client_method)
             if instrumentation_config[:suppress_internal_instrumentation]
               OpenTelemetry::Common::Utilities.untraced { super }
             else
@@ -30,7 +39,7 @@ module OpenTelemetry
             end.tap do |response|
               if (err = response.error)
                 span.record_exception(err)
-                span.status = Trace::Status.error(err)
+                span.status = Trace::Status.error(err.to_s)
               end
             end
           end
@@ -44,6 +53,51 @@ module OpenTelemetry
 
         def instrumentation_config
           AwsSdk::Instrumentation.instance.config
+        end
+
+        def service_name(context) # rubocop:disable Metrics/AbcSize
+          # Support aws-sdk v2.0.x, which 'metadata' has a setter method only
+          return context.client.class.to_s.split('::')[1] if ::Seahorse::Model::Api.instance_method(:metadata).parameters.length.positive?
+
+          context.client.class.api.metadata['serviceId'] || context.client.class.to_s.split('::')[1]
+        end
+
+        SEND_MESSAGE_CLIENT_METHODS = [SQS_SEND_MESSAGE, SQS_SEND_MESSAGE_BATCH, SNS_PUBLISH].freeze
+        def inject_context(context, client_method)
+          return unless SEND_MESSAGE_CLIENT_METHODS.include? client_method
+          return unless instrumentation_config[:inject_messaging_context]
+
+          if client_method == SQS_SEND_MESSAGE_BATCH
+            context.params[:entries].each do |entry|
+              entry[:message_attributes] ||= {}
+              OpenTelemetry.propagation.inject(entry[:message_attributes], setter: MessageAttributeSetter)
+            end
+          else
+            context.params[:message_attributes] ||= {}
+            OpenTelemetry.propagation.inject(context.params[:message_attributes], setter: MessageAttributeSetter)
+          end
+        end
+
+        def span_kind(client_method)
+          case client_method
+          when SQS_SEND_MESSAGE, SQS_SEND_MESSAGE_BATCH, SNS_PUBLISH
+            OpenTelemetry::Trace::SpanKind::PRODUCER
+          when SQS_RECEIVE_MESSAGE
+            OpenTelemetry::Trace::SpanKind::CONSUMER
+          else
+            OpenTelemetry::Trace::SpanKind::CLIENT
+          end
+        end
+
+        def span_name(context, client_method)
+          case client_method
+          when SQS_SEND_MESSAGE, SQS_SEND_MESSAGE_BATCH, SNS_PUBLISH
+            "#{MessagingHelper.queue_name(context)} send"
+          when SQS_RECEIVE_MESSAGE
+            "#{MessagingHelper.queue_name(context)} receive"
+          else
+            client_method
+          end
         end
       end
 
