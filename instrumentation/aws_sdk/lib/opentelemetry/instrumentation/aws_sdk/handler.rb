1a59955c5663446a47324c9193d82a7c4475337e
Yaniv Davidi <yaniv@aspecto.io>
2022-01-19 16:51:52 +0200

    chore(aws-sdk): improve service_name extraction

--- instrumentation/aws_sdk/lib/opentelemetry/instrumentation/aws_sdk/handler.rb
+++ instrumentation/aws_sdk/lib/opentelemetry/instrumentation/aws_sdk/handler.rb
@@ -17,7 +17,7 @@ module OpenTelemetry
         def call(context) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
           return super unless context
 
-          service_name = context.client.class.api.metadata['serviceId'] || context.client.class.to_s.split('::')[1] rescue context.client.class.to_s.split('::')[1] # rubocop:disable Style/RescueModifier
+          service_name = service_name(context)
           operation = context.operation&.name
           client_method = "#{service_name}.#{operation}"
           attributes = {
@@ -53,6 +53,13 @@ module OpenTelemetry
 
         def instrumentation_config
           AwsSdk::Instrumentation.instance.config
+        end
+
+        def service_name(context)
+          # Support aws-sdk v2.0.x, which 'metadata' has a setter method only
+          return context.client.class.to_s.split('::')[1] if ::Seahorse::Model::Api.instance_method(:metadata).parameters.length.positive?
+
+          context.client.class.api.metadata['serviceId'] || context.client.class.to_s.split('::')[1]
         end
 
         SEND_MESSAGE_CLIENT_METHODS = [SQS_SEND_MESSAGE, SQS_SEND_MESSAGE_BATCH, SNS_PUBLISH].freeze
