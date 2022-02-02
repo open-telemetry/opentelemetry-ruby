3dc46c9a9f43f81772af5096181532d92692dc7e
Robert Laurin <robert.laurin@shopify.com>
2022-02-02 15:33:25 -0500

    fix: prevent redis instrumentation from mutating the command

--- instrumentation/redis/lib/opentelemetry/instrumentation/redis/patches/client.rb
+++ instrumentation/redis/lib/opentelemetry/instrumentation/redis/patches/client.rb
@@ -71,11 +71,10 @@ module OpenTelemetry
               # and return the obfuscated command
               return 'AUTH ?' if command[0] == :auth
 
-              command[0] = command[0].to_s.upcase
               if config[:db_statement] == :obfuscate
-                command[0] + ' ?' * (command.size - 1)
+                command[0].to_s.upcase + ' ?' * (command.size - 1)
               else
-                command.join(' ')
+                command[0].to_s.upcase + " " +  command[1..-1].join(' ')
               end
             end.join("\n")
           end
