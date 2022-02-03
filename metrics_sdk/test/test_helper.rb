8e5470516d5b52f28ff48e9b242101c22444b7a5
Robert Laurin <robert.laurin@shopify.com>
2022-02-03 11:45:03 -0500

    fix: add metric install sdk tests

--- metrics_sdk/test/test_helper.rb
+++ metrics_sdk/test/test_helper.rb
@@ -12,4 +12,14 @@ require 'opentelemetry-metrics-sdk'
 require 'minitest/autorun'
 require 'pry'
 
-OpenTelemetry.logger = Logger.new(File::NULL)
+# reset_metrics_sdk is a test helper used to clear
+# SDK configuration state between calls
+def reset_metrics_sdk
+  OpenTelemetry.instance_variable_set(
+    :@meter_provider,
+    OpenTelemetry::Internal::ProxyMeterProvider.new
+  )
+
+  OpenTelemetry.logger = Logger.new(File::NULL)
+  OpenTelemetry.error_handler = nil
+end
