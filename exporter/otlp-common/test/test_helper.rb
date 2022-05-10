6c7f9d35790411aa1d39f98fa817bc4da0b362c7
Robert <robertlaurin@users.noreply.github.com>
2022-05-09 20:25:05 -0500

    Split otlp exporter (#1179)
    * refactor: extract common otlp exporter code

---
+++ exporter/otlp-common/test/test_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Copyright The OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'simplecov'
+SimpleCov.start
+
+require 'opentelemetry/sdk'
+require 'opentelemetry-test-helpers'
+require 'opentelemetry-exporter-otlp-common'
+
+require 'minitest/autorun'
+require 'webmock/minitest'
+
+require 'pry'
+
+OpenTelemetry.logger = Logger.new(File::NULL)
