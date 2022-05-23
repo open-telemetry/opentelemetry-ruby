bfb6210352639d078c73fdd7c9e4baf76ae3f596
Andrew Hayworth <ahayworth@gmail.com>
2022-05-23 12:47:39 -0500

    fixup: remove unneeded TODO
    Per Francis, this is actually by design...

--- api/test/opentelemetry/trace/tracer_provider_test.rb
+++ api/test/opentelemetry/trace/tracer_provider_test.rb
@@ -10,7 +10,6 @@ describe OpenTelemetry::Trace::TracerProvider do
   let(:tracer_provider) { OpenTelemetry::Trace::TracerProvider.new }
 
   describe '.tracer' do
-    # TODO: This test doesn't really do anything for the API tracer
     it 'returns the same tracer for the same arguments' do
       tracer1 = tracer_provider.tracer('component', '1.0', 'https://http.cat/404')
       tracer2 = tracer_provider.tracer('component', '1.0', 'https://http.cat/404')
