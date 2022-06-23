1268181e4289aceae19facfb47ad8f2cb08ebdf3
Robert <robertlaurin@users.noreply.github.com>
2022-06-23 15:00:21 -0500

    Update api/lib/opentelemetry/baggage/propagation/text_map_propagator.rb

--- api/lib/opentelemetry/baggage/propagation/text_map_propagator.rb
+++ api/lib/opentelemetry/baggage/propagation/text_map_propagator.rb
@@ -51,7 +51,7 @@ module OpenTelemetry
         #
         # @return [Context] context updated with extracted baggage, or the original context
         #   if extraction fails
-        def extract(carrier, context: Context.current, getter: Context::Propagation.text_map_getter) # rubocop:disable Metrics/AbcSize
+        def extract(carrier, context: Context.current, getter: Context::Propagation.text_map_getter)
           header = getter.get(carrier, BAGGAGE_KEY)
           return context if header.nil? || header.empty?
 
