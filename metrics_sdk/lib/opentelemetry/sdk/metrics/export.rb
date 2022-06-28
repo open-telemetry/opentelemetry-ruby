79605fddebf9b12e2be05d389b2a8023ef569c6b
Robert Laurin <robert.laurin@shopify.com>
2022-06-28 14:05:06 -0500

    Remove console exporter

--- metrics_sdk/lib/opentelemetry/sdk/metrics/export.rb
+++ metrics_sdk/lib/opentelemetry/sdk/metrics/export.rb
@@ -24,5 +24,4 @@ module OpenTelemetry
 end
 
 require 'opentelemetry/sdk/metrics/export/metric_reader'
-require 'opentelemetry/sdk/metrics/export/console_metric_exporter'
 require 'opentelemetry/sdk/metrics/export/in_memory_metric_pull_exporter'
