c261b6d603c42aa6c7977fc6bb03b4a349b348b6
Robert <robertlaurin@users.noreply.github.com>
2022-06-30 10:46:54 -0500

    fix: add hayworth comment
    Co-authored-by: Andrew Hayworth <ahayworth@gmail.com>

--- metrics_sdk/lib/opentelemetry/sdk/metrics/instrument/counter.rb
+++ metrics_sdk/lib/opentelemetry/sdk/metrics/instrument/counter.rb
@@ -27,6 +27,9 @@ module OpenTelemetry
           #   Array values must not contain nil elements and all elements must be of
           #   the same basic type (string, numeric, boolean).
           def add(increment, attributes: {})
+            # TODO: When the metrics SDK stabilizes and is merged into the main SDK,
+            # we can leverage the SDK Internal validation classes to enforce this:
+            # https://github.com/open-telemetry/opentelemetry-ruby/blob/6bec625ef49004f364457c26263df421526b60d6/sdk/lib/opentelemetry/sdk/internal.rb#L47
             if increment.negative?
               OpenTelemetry.logger.warn("#{@name} received a negative value")
             else
