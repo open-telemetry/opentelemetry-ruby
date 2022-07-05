672739120a605c5c14afc48e5aa07d1320443355
Robert <robertlaurin@users.noreply.github.com>
2022-07-05 14:22:16 -0500

    Metrics SDK synchronous instruments (#1108)
    * WIP: Metrics SDK implementation
    
    * Extend the existing OpenTelemetry SDK
    
    * fix: remove duplicate mutex instantiation
    
    * feat: add api metrics measurement
    
    * fix: add metric install sdk tests
    
    * fix: comments
    
    Co-authored-by: Francis Bogsanyi <francis.bogsanyi@shopify.com>
    
    * fix: add meter provider implementation and tests
    
    * fix: ruby style
    
    * Implement SDK instrument creation
    
    * fix: handling of immutable descriptions
    
    * wip: safety commit
    
    * chore: rubocops
    
    * fix: add missing require
    
    * Synchronous instruments
    
    * chore: remove exporter work from PR
    
    * Metric Stream refactor
    
    * Remove metrics test helper
    
    * feedback fixups
    
    * Remove console exporter
    
    * Add integration test assertions
    
    * fix: broken test assert
    
    * chore: appease the cop
    
    * chore: appease the cop
    
    * fix: francis feedback
    
    Co-authored-by: Francis Bogsanyi <francis.bogsanyi@shopify.com>

---
+++ metrics_sdk/lib/opentelemetry-metrics-sdk.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Copyright The OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'opentelemetry/sdk'
+require 'opentelemetry-metrics-api'
+require 'opentelemetry/sdk/metrics'
