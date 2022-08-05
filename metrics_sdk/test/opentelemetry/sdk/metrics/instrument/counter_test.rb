35cc184299c6a1b3f1f46607200ea7e52d65b028
Robert Laurin <robert.laurin@shopify.com>
2022-08-05 14:06:59 -0500

    fix: add counter instrument tests

--- metrics_sdk/test/opentelemetry/sdk/metrics/instrument/counter_test.rb
+++ metrics_sdk/test/opentelemetry/sdk/metrics/instrument/counter_test.rb
@@ -7,4 +7,26 @@
 require 'test_helper'
 
 describe OpenTelemetry::SDK::Metrics::Instrument::Counter do
+  let(:metric_exporter) { OpenTelemetry::SDK::Metrics::Export::InMemoryMetricPullExporter.new }
+  let(:meter) { OpenTelemetry.meter_provider.meter('test') }
+  let(:counter) { meter.create_counter('counter', unit: 'smidgen', description: 'a small amount of something') }
+
+  before do
+    reset_metrics_sdk
+    OpenTelemetry::SDK.configure
+    OpenTelemetry.meter_provider.add_metric_reader(metric_exporter)
+  end
+
+  it 'counts' do
+    counter.add(1, attributes: { 'foo' => 'bar' })
+    metric_exporter.pull
+    last_snapshot = metric_exporter.metric_snapshots.last
+
+    _(last_snapshot[0].name).must_equal('counter')
+    _(last_snapshot[0].unit).must_equal('smidgen')
+    _(last_snapshot[0].description).must_equal('a small amount of something')
+    _(last_snapshot[0].instrumentation_library.name).must_equal('test')
+    _(last_snapshot[0].data_points[0].value).must_equal(1)
+    _(last_snapshot[0].data_points[0].attributes).must_equal('foo' => 'bar')
+  end
 end
