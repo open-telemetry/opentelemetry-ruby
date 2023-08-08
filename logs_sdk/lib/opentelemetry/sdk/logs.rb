e9c3f58864b2a9085170ed443262c96fb0226f2b
Kayla Reopelle <kreopelle@newrelic.com>
2023-08-08 08:10:32 -0700

    feat: Create opentelemetry-logs-sdk gem

---
+++ logs_sdk/lib/opentelemetry/sdk/logs.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Copyright The OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require_relative 'logs/version'
+
+module OpenTelemetry
+  module SDK
+    # The Logs module contains the OpenTelemetry logs reference
+    # implementation.
+    module Logs
+    end
+  end
+end
---
+++ logs_sdk/test/test_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Copyright The OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'simplecov'
+SimpleCov.start { enable_coverage :branch }
+SimpleCov.minimum_coverage 85
+
+require 'opentelemetry-logs-api'
+require 'minitest/autorun'
+
+OpenTelemetry.logger = Logger.new(File::NULL)
