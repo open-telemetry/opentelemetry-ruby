a5ed0b4c8f31dc61028618eacf1a167840758a08
Kayla Reopelle (she/her) <87386821+kaylareopelle@users.noreply.github.com>
2023-08-24 14:28:07 -0700

    feat: Create opentelemetry-logs-sdk gem (#1511)

---
+++ logs_sdk/CHANGELOG.md
@@ -0,0 +1,1 @@
+# Release History: opentelemetry-logs-sdk
---
+++ logs_sdk/lib/opentelemetry-logs-sdk.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Copyright The OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'opentelemetry/sdk/logs'
+require 'opentelemetry/sdk/logs/version'
