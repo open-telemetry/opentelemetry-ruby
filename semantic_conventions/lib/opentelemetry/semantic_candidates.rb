9e4045ff02fb5b91a6a8ff06d5d3fe1943ed5e86
Robb Kidd <robbkidd@honeycomb.io>
2024-07-12 11:57:54 -0400

    add/expand doc comments on top-level modules
    Explain the auto-generated situation.

--- semantic_conventions/lib/opentelemetry/semantic_candidates.rb
+++ semantic_conventions/lib/opentelemetry/semantic_candidates.rb
@@ -8,19 +8,12 @@ module OpenTelemetry
   # OpenTelemetry semantic convention attribute names as constants.
   # These are auto-generated from source YAML in {https://github.com/open-telemetry/semantic-conventions the semantic-conventions repository}.
   #
-  # Except for the {Resource} and {Trace} modules, constants in this namespace have been declared stable by the OpenTelemetry semantic conventions working group.
+  # The constants in this namespace are all of the attribute names at all stability levels present in the OpenTelemetry semantic conventions as of {SemanticConventions::VERSION}.
   #
   # @note The constants here ought to remain within major versions of this library.
   #   However, there risk with auto-generated code.
   #   The maintainers try to prevent constants disappearing, but we cannot gaurantee this.
   #   We strongly recommend that any constants you use in your code are exercised in your test suite to catch missing constants before release or production runtime.
-  module SemanticConventions
+  module SemanticCandidates
   end
 end
-
-# TODO: test to make sure the trace and resource constants are present in SemanticCandidates
-# TODO: test to make sure the SemanticConventions (stable) constants are all still present in the SemanticCandidates constants
-# TODO: remove these convenience requires in the next major version
-require_relative 'semantic_conventions/trace'
-require_relative 'semantic_conventions/resource'
-# TODO: we're not going to add any more convenience requires here; require directly what you use
