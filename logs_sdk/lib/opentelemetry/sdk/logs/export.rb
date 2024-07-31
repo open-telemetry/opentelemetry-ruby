7dfe64929aa96c0fa1d7dc8d9d268c940e990ba2
Kayla Reopelle <kreopelle@newrelic.com>
2024-07-31 12:15:00 -0700

    feat: Add log record exporter interface

--- logs_sdk/lib/opentelemetry/sdk/logs/export.rb
+++ logs_sdk/lib/opentelemetry/sdk/logs/export.rb
@@ -7,6 +7,9 @@
 module OpenTelemetry
   module SDK
     module Logs
+      ExportError = Class.new(OpenTelemetry::Error)
+      # The export module contains result codes for LoggerProvider#force_flush
+      # and LoggerProvider#shutdown
       # The Export module contains result codes for exporters
       module Export
         # The operation finished successfully.
@@ -21,3 +24,5 @@ module OpenTelemetry
     end
   end
 end
+
+require_relative 'export/log_record_exporter'
