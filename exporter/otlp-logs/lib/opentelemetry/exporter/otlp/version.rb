d44183dfadce398803d2a4fc4915c10616385942
Kayla Reopelle <kreopelle@newrelic.com>
2024-08-26 15:20:08 -0700

    feat: Create OTLP Logs Exporter

---
+++ exporter/otlp-logs/lib/opentelemetry/exporter/otlp/version.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Copyright The OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+module OpenTelemetry
+  module Exporter
+    module OTLP
+      ## Current OpenTelemetry OTLP logs exporter version
+      VERSION = '0.1.0'
+    end
+  end
+end
