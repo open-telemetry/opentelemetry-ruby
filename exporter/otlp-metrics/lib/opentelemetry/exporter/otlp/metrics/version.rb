85774807440f2aa6956cdfa56b89fb8fc9c069cb
Daniel Azuma <dazuma@gmail.com>
2024-10-15 15:05:33 +0000

    release: Release 4 gems
    * opentelemetry-exporter-otlp 0.29.1 (was 0.29.0)
    * opentelemetry-exporter-otlp-metrics 0.2.0 (was 0.1.0)
    * opentelemetry-exporter-zipkin 0.23.2 (was 0.23.1)
    * opentelemetry-metrics-sdk 0.3.0 (was 0.2.0)

--- exporter/otlp-metrics/lib/opentelemetry/exporter/otlp/metrics/version.rb
+++ exporter/otlp-metrics/lib/opentelemetry/exporter/otlp/metrics/version.rb
@@ -9,7 +9,7 @@ module OpenTelemetry
     module OTLP
       module Metrics
         ## Current OpenTelemetry OTLP exporter version
-        VERSION = '0.1.0'
+        VERSION = '0.2.0'
       end
     end
   end
--- metrics_sdk/CHANGELOG.md
+++ metrics_sdk/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release History: opentelemetry-metrics-sdk
 
+### v0.3.0 / 2024-10-15
+
+* ADDED: Add basic metrics view
+
 ### v0.2.0 / 2024-08-27
 
 * ADDED: Add basic periodic exporting metric_reader
