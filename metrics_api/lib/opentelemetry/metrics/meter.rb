060aef51473be090ff8ed413b703ffd284e0fba2
Kayla Reopelle <87386821+kaylareopelle@users.noreply.github.com>
2025-01-08 12:37:09 -0800

    Update metrics_api/lib/opentelemetry/metrics/meter.rb

--- metrics_api/lib/opentelemetry/metrics/meter.rb
+++ metrics_api/lib/opentelemetry/metrics/meter.rb
@@ -83,7 +83,6 @@ module OpenTelemetry
       def create_gauge(name, unit: nil, description: nil)
         create_instrument(:gauge, name, unit, description, nil) { GAUGE }
       end
-      
       # UpDownCounter is a synchronous Instrument which supports increments and decrements.
       #
       # With this api call:
