048f574ebb4414d819a34fe254e194e3aadb476f
Josh Westbrook <josh.westbrook@storable.com>
2025-04-15 10:11:33 -0500

    feat: Allow specifying aggregation for SynchronousInstruments

--- metrics_sdk/lib/opentelemetry/sdk/metrics/instrument/synchronous_instrument.rb
+++ metrics_sdk/lib/opentelemetry/sdk/metrics/instrument/synchronous_instrument.rb
@@ -11,19 +11,20 @@ module OpenTelemetry
         # {SynchronousInstrument} contains the common functionality shared across
         # the synchronous instruments SDK instruments.
         class SynchronousInstrument
-          def initialize(name, unit, description, instrumentation_scope, meter_provider)
+          def initialize(name, unit, description, instrumentation_scope, meter_provider, aggregation: default_aggregation)
             @name = name
             @unit = unit
             @description = description
             @instrumentation_scope = instrumentation_scope
             @meter_provider = meter_provider
+            @aggregation = aggregation
             @metric_streams = []
 
             meter_provider.register_synchronous_instrument(self)
           end
 
           # @api private
-          def register_with_new_metric_store(metric_store, aggregation: default_aggregation)
+          def register_with_new_metric_store(metric_store, aggregation: nil)
             ms = OpenTelemetry::SDK::Metrics::State::MetricStream.new(
               @name,
               @description,
@@ -31,7 +32,7 @@ module OpenTelemetry
               instrument_kind,
               @meter_provider,
               @instrumentation_scope,
-              aggregation
+              aggregation || @aggregation
             )
             @metric_streams << ms
             metric_store.add_metric_stream(ms)
