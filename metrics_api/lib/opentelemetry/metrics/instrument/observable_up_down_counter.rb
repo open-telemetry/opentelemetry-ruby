9008083ab62e5d60fef6e343f587e8fccfc21b0b
Xuan <112967240+xuan-cao-swi@users.noreply.github.com>
2025-07-01 12:58:32 -0400

    Update metrics_api/lib/opentelemetry/metrics/instrument/observable_up_down_counter.rb
    Co-authored-by: Kayla Reopelle <87386821+kaylareopelle@users.noreply.github.com>

--- metrics_api/lib/opentelemetry/metrics/instrument/observable_up_down_counter.rb
+++ metrics_api/lib/opentelemetry/metrics/instrument/observable_up_down_counter.rb
@@ -10,7 +10,6 @@ module OpenTelemetry
       # No-op implementation of ObservableUpDownCounter.
       class ObservableUpDownCounter
         # Observe the ObservableUpDownCounter with fixed timeout duration.
-        # Everytime observe, the value should be sent to backend through exporter
         #
         # @param [int] timeout The timeout duration for callback to run, which MUST be a non-negative numeric value.
         # @param [Hash{String => String, Numeric, Boolean, Array<String, Numeric, Boolean>}] attributes
