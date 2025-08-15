329ae8a00be47173b202b565015d4855027adbc0
Kayla Reopelle <kreopelle@newrelic.com>
2025-08-15 13:49:21 -0700

    feat: Add OTEL_LOGRECORD_ATTRIBUTE_* env vars
    Previously, the configuration option for managing log record attributes
    spelled log record with an underscore, `LOG_RECORD`. However, the spec
    does not use an underscore, `LOGRECORD`. Now, both spellings will be
    checked to set the value, with `LOGRECORD` taking precedence.

--- logs_sdk/lib/opentelemetry/sdk/logs/log_record_limits.rb
+++ logs_sdk/lib/opentelemetry/sdk/logs/log_record_limits.rb
@@ -20,11 +20,13 @@ module OpenTelemetry
         # @return [LogRecordLimits] with the desired values.
         # @raise [ArgumentError] if any of the max numbers are not positive.
         def initialize(attribute_count_limit: Integer(OpenTelemetry::Common::Utilities.config_opt(
+                                                        'OTEL_LOGRECORD_ATTRIBUTE_COUNT_LIMIT',
                                                         'OTEL_LOG_RECORD_ATTRIBUTE_COUNT_LIMIT',
                                                         'OTEL_ATTRIBUTE_COUNT_LIMIT',
                                                         default: 128
                                                       )),
                        attribute_length_limit: OpenTelemetry::Common::Utilities.config_opt(
+                         'OTEL_LOGRECORD_ATTRIBUTE_VALUE_LENGTH_LIMIT',
                          'OTEL_LOG_RECORD_ATTRIBUTE_VALUE_LENGTH_LIMIT',
                          'OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT'
                        ))
