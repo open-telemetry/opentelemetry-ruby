55b6ce8e2d7f7c1c36e0654b058d4d7e12fd2568
nikhilmantri0902 <nikhil.mantri1999@gmail.com>
2025-09-09 16:40:42 +0530

    chore: implemented changes in expected file and improved code post review

--- exporter/otlp-common/CHANGELOG.md
+++ exporter/otlp-common/CHANGELOG.md
@@ -2,5 +2,5 @@
 
 ### Unreleased
 
-* FEAT: Add span flags support for isRemote property in OTLP exporter [#1917](https://github.com/open-telemetry/opentelemetry-ruby/pull/1917)
+* FEAT: Add span flags support for isRemote property in OTLP exporter [#1917](https://github.com/open-telemetry/opentelemetry-ruby/pull/1917) (Note: This was implemented in the main OTLP exporter, not in this common module)
 * REFACTORED: opentelemetry-exporter-otlp to extract common code here. Refer to its changelog for additional history. [#1179](https://github.com/open-telemetry/opentelemetry-ruby/pull/1179)
--- exporter/otlp/test/opentelemetry/exporter/otlp/exporter_test.rb
+++ exporter/otlp/test/opentelemetry/exporter/otlp/exporter_test.rb
@@ -779,4 +779,112 @@ describe OpenTelemetry::Exporter::OTLP::Exporter do
       end
     end
   end
+
+  describe 'span flags' do
+    let(:exporter) { OpenTelemetry::Exporter::OTLP::Exporter.new }
+    let(:trace_id) { OpenTelemetry::Trace.generate_trace_id }
+    let(:span_id) { OpenTelemetry::Trace.generate_span_id }
+    let(:parent_span_id) { OpenTelemetry::Trace.generate_span_id }
+    let(:resource) { OpenTelemetry::SDK::Resources::Resource.create('service.name' => 'test-service') }
+    let(:instrumentation_scope) { OpenTelemetry::SDK::InstrumentationScope.new('test-lib', '1.0.0') }
+
+    describe 'build_span_flags' do
+      it 'sets flags to 0x100 for local parent span context' do
+        flags = exporter.send(:build_span_flags, false, 0)
+        _(flags).must_equal(0x100) # SPAN_FLAGS_CONTEXT_HAS_IS_REMOTE_MASK
+      end
+
+      it 'sets flags to 0x300 for remote parent span context' do
+        flags = exporter.send(:build_span_flags, true, 0)
+        _(flags).must_equal(0x300) # SPAN_FLAGS_CONTEXT_HAS_IS_REMOTE_MASK | SPAN_FLAGS_CONTEXT_IS_REMOTE_MASK
+      end
+
+      it 'preserves base trace flags' do
+        flags = exporter.send(:build_span_flags, false, 0x01) # SAMPLED flag
+        _(flags).must_equal(0x101) # 0x01 (SAMPLED) | 0x100 (HAS_IS_REMOTE_MASK)
+      end
+    end
+
+    describe 'as_otlp_span with flags' do
+      it 'sets flags to 0x100 for local parent span context' do
+        span_data = create_span_data(parent_span_is_remote: false)
+        span = exporter.send(:as_otlp_span, span_data)
+        _(span.flags).must_equal(0x100) # SPAN_FLAGS_CONTEXT_HAS_IS_REMOTE_MASK
+      end
+
+      it 'sets flags to 0x300 for remote parent span context' do
+        span_data = create_span_data(parent_span_is_remote: true)
+        span = exporter.send(:as_otlp_span, span_data)
+        _(span.flags).must_equal(0x300) # SPAN_FLAGS_CONTEXT_HAS_IS_REMOTE_MASK | SPAN_FLAGS_CONTEXT_IS_REMOTE_MASK
+      end
+    end
+
+    describe 'as_otlp_span with link flags' do
+      it 'sets link flags to 0x100 for local link context' do
+        local_span_context = OpenTelemetry::Trace::SpanContext.new(trace_id: trace_id, span_id: parent_span_id, remote: false)
+        local_link = create_link(local_span_context)
+        span_data = create_span_data(links: [local_link])
+        span = exporter.send(:as_otlp_span, span_data)
+        _(span.links.first.flags).must_equal(0x100) # SPAN_FLAGS_CONTEXT_HAS_IS_REMOTE_MASK
+      end
+
+      it 'sets link flags to 0x300 for remote link context' do
+        remote_span_context = OpenTelemetry::Trace::SpanContext.new(trace_id: trace_id, span_id: parent_span_id, remote: true)
+        remote_link = create_link(remote_span_context)
+        span_data = create_span_data(links: [remote_link])
+        span = exporter.send(:as_otlp_span, span_data)
+        _(span.links.first.flags).must_equal(0x300) # SPAN_FLAGS_CONTEXT_HAS_IS_REMOTE_MASK | SPAN_FLAGS_CONTEXT_IS_REMOTE_MASK
+      end
+    end
+
+    describe 'export with flags' do
+      it 'includes flags in exported spans' do
+        span_data = create_span_data(parent_span_is_remote: true)
+        encoded_data = exporter.send(:encode, [span_data])
+        decoded = Opentelemetry::Proto::Collector::Trace::V1::ExportTraceServiceRequest.decode(encoded_data)
+        exported_span = decoded.resource_spans.first.scope_spans.first.spans.first
+        _(exported_span.flags).must_equal(0x300) # SPAN_FLAGS_CONTEXT_HAS_IS_REMOTE_MASK | SPAN_FLAGS_CONTEXT_IS_REMOTE_MASK
+      end
+
+      it 'includes flags in exported links' do
+        remote_span_context = OpenTelemetry::Trace::SpanContext.new(trace_id: trace_id, span_id: parent_span_id, remote: true)
+        remote_link = create_link(remote_span_context)
+        span_data = create_span_data(links: [remote_link])
+        encoded_data = exporter.send(:encode, [span_data])
+        decoded = Opentelemetry::Proto::Collector::Trace::V1::ExportTraceServiceRequest.decode(encoded_data)
+        exported_link = decoded.resource_spans.first.scope_spans.first.spans.first.links.first
+        _(exported_link.flags).must_equal(0x300) # SPAN_FLAGS_CONTEXT_HAS_IS_REMOTE_MASK | SPAN_FLAGS_CONTEXT_IS_REMOTE_MASK
+      end
+    end
+
+    private
+
+    def create_span_data(parent_span_is_remote: false, links: [])
+      OpenTelemetry::SDK::Trace::SpanData.new(
+        'test-span',                    # name
+        :internal,                      # kind
+        OpenTelemetry::Trace::Status::OK, # status
+        parent_span_id,                 # parent_span_id
+        0,                             # total_recorded_attributes
+        0,                             # total_recorded_events
+        links.size,                    # total_recorded_links
+        Time.now.to_i * 1_000_000_000, # start_timestamp
+        Time.now.to_i * 1_000_000_000, # end_timestamp
+        {},                            # attributes
+        links,                         # links
+        [],                            # events
+        resource,                      # resource
+        instrumentation_scope,         # instrumentation_scope
+        span_id,                       # span_id
+        trace_id,                      # trace_id
+        OpenTelemetry::Trace::TraceFlags::DEFAULT, # trace_flags
+        OpenTelemetry::Trace::Tracestate.new, # tracestate
+        parent_span_is_remote          # parent_span_is_remote
+      )
+    end
+
+    def create_link(span_context)
+      OpenTelemetry::Trace::Link.new(span_context, { 'link-attribute' => 'link-value' })
+    end
+  end
 end
