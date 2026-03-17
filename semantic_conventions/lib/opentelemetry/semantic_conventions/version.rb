7fcecd603c918a1cfe00a671c4d44e1a24625e85
Robb Kidd <robbkidd@honeycomb.io>
2026-03-17 19:21:18 -0400

    Maybe some comments about how these differ?

--- semantic_conventions/lib/opentelemetry/semantic_conventions/version.rb
+++ semantic_conventions/lib/opentelemetry/semantic_conventions/version.rb
@@ -6,7 +6,9 @@
 
 module OpenTelemetry
   module SemanticConventions
+    # Version of the OpenTelemetry Semantic Conventions from which this library was generated.
     SPEC_VERSION = '1.37.0'
+    # Release version of this gem.
     VERSION = '1.36.0'
   end
 end
