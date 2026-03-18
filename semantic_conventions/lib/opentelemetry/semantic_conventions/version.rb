1ec42e931948c22043fe0ea305b45e9bdbcabc94
Robb Kidd <robbkidd@honeycomb.io>
2026-03-18 13:49:03 -0400

    docs: update words about these version constants
    Better directions for how to hold this update process.

--- semantic_conventions/lib/opentelemetry/semantic_conventions/version.rb
+++ semantic_conventions/lib/opentelemetry/semantic_conventions/version.rb
@@ -8,7 +8,7 @@ module OpenTelemetry
   module SemanticConventions
     # Version of the OpenTelemetry Semantic Conventions from which this library was generated.
     SPEC_VERSION = '1.37.0'
-    # Release version of this gem.
+    # Release version of this gem. May not match SPEC_VERSION until gem is released after a spec update.
     VERSION = '1.36.0'
   end
 end
