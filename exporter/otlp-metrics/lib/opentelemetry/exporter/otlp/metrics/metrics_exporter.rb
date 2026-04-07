f81fbeeae1d18c06d1662bbb1c4162ad8e2b8e20
Xuan <112967240+xuan-cao-swi@users.noreply.github.com>
2026-04-07 07:13:35 -0400

    feat: add basic support for metrics exemplar (#1609)
    * feat: exemplar init commit
    
    * feat: add test case and use exemplar in all synchronous counter
    
    * feat: Environment variable OTEL_METRICS_EXEMPLAR_FILTER should have higher precedence
    
    * feat: exemplar add more test and lint
    
    * lint
    
    * fix test case
    
    * lint on metrics api
    
    * init update
    
    * test case pass
    
    * update test case
    
    * add support for metrics exporter and fix some bugs
    
    * add view support for exemplar
    
    * refactor the exemplar structure
    
    * init pass, need more later
    
    * update
    
    * add unsaved sum
    
    * update test case
    
    * avoid use gc for test case
    
    * add fix number of size of exemplar bucket for test
    
    * skip for now
    
    * merge and fix one typo on filtered_attributes for exemplar
    
    * update api doc
    
    * Apply suggestions from code review
    
    Co-authored-by: Kayla Reopelle <87386821+kaylareopelle@users.noreply.github.com>
    
    * revision
    
    ---------
    
    Co-authored-by: Kayla Reopelle <87386821+kaylareopelle@users.noreply.github.com>

--- exporter/otlp-metrics/lib/opentelemetry/exporter/otlp/metrics/metrics_exporter.rb
+++ exporter/otlp-metrics/lib/opentelemetry/exporter/otlp/metrics/metrics_exporter.rb
@@ -298,7 +298,7 @@ module OpenTelemetry
               sum: hdp.sum,
               bucket_counts: hdp.bucket_counts,
               explicit_bounds: hdp.explicit_bounds,
-              exemplars: hdp.exemplars,
+              exemplars: as_otlp_exemplars(hdp.exemplars),
               min: hdp.min,
               max: hdp.max
             )
@@ -322,7 +322,7 @@ module OpenTelemetry
                 bucket_counts: ehdp.negative.counts
               ),
               flags: ehdp.flags,
-              exemplars: ehdp.exemplars,
+              exemplars: as_otlp_exemplars(ehdp.exemplars),
               min: ehdp.min,
               max: ehdp.max,
               zero_threshold: ehdp.zero_threshold
@@ -334,7 +334,7 @@ module OpenTelemetry
               attributes: ndp.attributes.map { |k, v| as_otlp_key_value(k, v) },
               start_time_unix_nano: ndp.start_time_unix_nano,
               time_unix_nano: ndp.time_unix_nano,
-              exemplars: ndp.exemplars # exemplars not implemented yet from metrics sdk
+              exemplars: as_otlp_exemplars(ndp.exemplars)
             }
 
             if ndp.value.is_a?(Float)
@@ -344,6 +344,30 @@ module OpenTelemetry
             end
 
             Opentelemetry::Proto::Metrics::V1::NumberDataPoint.new(**args)
+          end
+
+          def as_otlp_exemplars(exemplars)
+            exemplars&.map { |ex| as_otlp_exemplar(ex) } || []
+          end
+
+          def as_otlp_exemplar(exemplar)
+            args = {
+              time_unix_nano: exemplar.time_unix_nano,
+              span_id: exemplar.span_id,
+              trace_id: exemplar.trace_id
+            }
+
+            # Add filtered_attributes if present
+            args[:filtered_attributes] = exemplar.filtered_attributes.map { |k, v| as_otlp_key_value(k, v) } if exemplar.filtered_attributes
+
+            # Set value based on type
+            if exemplar.value.is_a?(Float)
+              args[:as_double] = exemplar.value
+            else
+              args[:as_int] = exemplar.value
+            end
+
+            Opentelemetry::Proto::Metrics::V1::Exemplar.new(**args)
           end
 
           # may not need this
