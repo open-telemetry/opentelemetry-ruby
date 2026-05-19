6e7dbaf00206d81b969f603e4af0f39efd895179
otelbot <197425009+otelbot@users.noreply.github.com>
2026-05-19 16:13:09 +0000

    release: Release opentelemetry-semantic_conventions 1.38.0 (was 1.37.1)

--- semantic_conventions/lib/opentelemetry/semantic_conventions/version.rb
+++ semantic_conventions/lib/opentelemetry/semantic_conventions/version.rb
@@ -9,6 +9,6 @@ module OpenTelemetry
     # Version of the OpenTelemetry Semantic Conventions from which this library was generated.
     SPEC_VERSION = '1.38.0'
     # Release version of this gem. May not match SPEC_VERSION until gem is released after a spec update.
-    VERSION = '1.37.1'
+    VERSION = '1.38.0'
   end
 end
